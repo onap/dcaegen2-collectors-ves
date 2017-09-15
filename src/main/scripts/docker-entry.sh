c6ef73bb783dfce2d640e6058b45c81a0d241c1b
Vijay <vv770d@att.com>
2017-09-15 19:46:07 +0000

    vescollector standalone docker support
    Issue-Id:DCAEGEN2-82
    Change-Id: Id309d0af08c8beb9c18dce08dc51344e60f494a4
    Signed-off-by: Vijay <vv770d@att.com>

--- src/main/scripts/docker-entry.sh
+++ src/main/scripts/docker-entry.sh
@@ -18,10 +18,19 @@
 # limitations under the License.
 # ============LICENSE_END=========================================================
 ###
-#echo \$COLLECTOR_IP  \$(hostname).dcae.simpledemo.openecomp.org >> /etc/hosts
+
+if [ ! -z "$COLLECTOR_IP" ]; then
+        echo $COLLECTOR_IP  $(hostname).dcae.simpledemo.openecomp.org >> /etc/hosts
+fi
+
+if [ ! -z "$DMAAPHOST" ]; then
+        echo $DMAAPHOST  onap.dmaap.org >> /etc/hosts
+else
+	echo "DMAAPHOST ENV NOT SET!! PUBLISH WILL NOT BE SUPPORTED"
+fi
+
 if [ -z "$CONSUL_HOST" ] || [ -z "$CONFIG_BINDING_SERVICE" ] || [ -z "$HOSTNAME" ]; then
-                echo "INFO: USING STANDARD ALONE CONFIGURATION SETUP; DMAAP PUBLISH NOT SUPPORTED"
-               # /opt/app/manager/start-manager.sh
+                echo "INFO: USING STANDARD ALONE CONFIGURATION SETUP"
 else
                 echo "INFO: USING DCAEGEN2 CONTROLLER"
 fi
