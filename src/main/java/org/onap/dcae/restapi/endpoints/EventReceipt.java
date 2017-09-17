4e9e25c0bd99bd686648d7dd1dc64ad98c49f5b4
Bharat saraswal <bharat.saraswal@huawei.com>
2017-09-17 14:15:01 +0530

    Fixing sonar issues.
    removed redundant code.
    
    Issue-Id:DCAEGEN2-92
    
    Change-Id: I98a64dcfb1d7d40617324ee0fc6d1a70e368cff9
    Signed-off-by: Bharat saraswal <bharat.saraswal@huawei.com>

--- src/main/java/org/onap/dcae/restapi/endpoints/EventReceipt.java
+++ src/main/java/org/onap/dcae/restapi/endpoints/EventReceipt.java
@@ -20,267 +20,259 @@
 
 package org.onap.dcae.restapi.endpoints;
 
-import java.io.FileReader;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.UUID;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.JSONTokener;
-import org.onap.dcae.commonFunction.CommonStartup;
-import org.onap.dcae.commonFunction.CustomExceptionLoader;
-import org.onap.dcae.commonFunction.VESLogger;
-import org.onap.dcae.commonFunction.CommonStartup.QueueFullException;
-
 import com.att.nsa.apiServer.endpoints.NsaBaseEndpoint;
 import com.att.nsa.clock.SaClock;
 import com.att.nsa.drumlin.service.framework.context.DrumlinRequestContext;
 import com.att.nsa.drumlin.service.standards.HttpStatusCodes;
 import com.att.nsa.drumlin.service.standards.MimeTypes;
 import com.att.nsa.logging.LoggingContext;
-
 import com.att.nsa.logging.log4j.EcompFields;
 import com.att.nsa.security.db.simple.NsaSimpleApiKey;
-
 import com.google.gson.JsonParser;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import org.onap.dcae.commonFunction.CommonStartup;
+import org.onap.dcae.commonFunction.CommonStartup.QueueFullException;
+import org.onap.dcae.commonFunction.CustomExceptionLoader;
+import org.onap.dcae.commonFunction.VESLogger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EventReceipt extends NsaBaseEndpoint {
-	static String valresult = null;
-	static JSONObject customerror = null;
-	
-	private static final Logger log = LoggerFactory.getLogger(EventReceipt.class);
 
-	
-	public static void receiveVESEvent(DrumlinRequestContext ctx) throws IOException {
-		// the request body carries events. assume for now it's an array
-		// of json objects that fits in memory. (See cambria's parsing for
-		// handling large messages)
+    private static final Logger log = LoggerFactory.getLogger(EventReceipt.class);
+    private static final String MESSAGE = " Message:";
+    static String valresult;
+    static JSONObject customerror;
 
-		NsaSimpleApiKey retkey = null;
 
-		JSONArray jsonArray = null;
-		JSONArray jsonArrayMod = new JSONArray();
-		JSONObject event = null;
-		JSONObject jsonObject = null;
-		FileReader fr = null;
-		InputStream istr = null;
-		int arrayflag = 0;
-		String vesversion = null;
-		
-		
+    public static void receiveVESEvent(DrumlinRequestContext ctx) {
+        // the request body carries events. assume for now it's an array
+        // of json objects that fits in memory. (See cambria's parsing for
+        // handling large messages)
 
-		try {
-			//System.out.print("Version string:" + version);
+        NsaSimpleApiKey retkey = null;
 
-			// String br = new BufferedReader(new InputStreamReader(ctx.request().getBodyStream())).readLine();
-			// JsonElement msg = new JsonParser().parse(new BufferedReader(new InputStreamReader(ctx.request().getBodyStream())).readLine());
-			// jsonArray = new JSONArray ( new JSONTokener ( ctx.request().getBodyStream () ) );
+        JSONArray jsonArray;
+        JSONArray jsonArrayMod = new JSONArray();
+        JSONObject event;
+        JSONObject jsonObject;
+        FileReader fr = null;
+        InputStream istr = null;
+        int arrayFlag = 0;
+        String vesVersion = null;
 
-			
-			log.debug ("Request recieved :" + ctx.request().getRemoteAddress());
-			istr = ctx.request().getBodyStream();
-			jsonObject = new JSONObject(new JSONTokener(istr));
-			
-			log.info("ctx getPathInContext: " + ctx.request().getPathInContext());
-	        Pattern p = Pattern.compile("(v\\d+)");
-	        Matcher m = p.matcher(ctx.request().getPathInContext());
-	        
-	        if (m.find()) {
-	        	log.info("VES version:" + m.group());
-	        	vesversion = m.group();
-	        }
-	        if (ctx.request().getPathInContext().contains("eventBatch"))
-	        {
-	        	CommonStartup.inlog.info(ctx.request().getRemoteAddress() + "VES Batch Input Messsage: " + jsonObject);
-				log.info(ctx.request().getRemoteAddress() + "VES Batch Input Messsage: " + jsonObject);
-	        	arrayflag = 1;
-	        }
-	        else
-	        {
-	        	CommonStartup.inlog.info(ctx.request().getRemoteAddress() + "Input Messsage: " + jsonObject);
-				log.info(ctx.request().getRemoteAddress() + "Input Messsage: " + jsonObject);
+        try {
+            //System.out.print("Version string:" + version);
 
-	        }
+            // String br = new BufferedReader(new InputStreamReader(ctx.request().getBodyStream())).readLine();
+            // JsonElement msg = new JsonParser().parse(new BufferedReader(new InputStreamReader(ctx.request().getBodyStream())).readLine());
+            // jsonArray = new JSONArray ( new JSONTokener ( ctx.request().getBodyStream () ) );
 
-			
-			final UUID uuid = java.util.UUID.randomUUID();
-			LoggingContext localLC = VESLogger.getLoggingContextForThread(uuid);
-			localLC .put ( EcompFields.kBeginTimestampMs, SaClock.now () );
+            log.debug("Request recieved :" + ctx.request().getRemoteAddress());
+            istr = ctx.request().getBodyStream();
+            jsonObject = new JSONObject(new JSONTokener(istr));
 
-			
-				
-			try {
-				if (CommonStartup.authflag == 1) {
-					retkey = NsaBaseEndpoint.getAuthenticatedUser(ctx);
-				}
-			} catch (NullPointerException x) {
-				log.info(
-						"Invalid user request " + ctx.request().getContentType() + " Message:" + jsonObject.toString());
-				CommonStartup.eplog.info("EVENT_RECEIPT_FAILURE: Unauthorized user" + x.toString());
-				respondWithCustomMsginJson(ctx, HttpStatusCodes.k401_unauthorized, "Invalid user");
-				return;
-			}
+            log.info("ctx getPathInContext: " + ctx.request().getPathInContext());
+            Pattern p = Pattern.compile("(v\\d+)");
+            Matcher m = p.matcher(ctx.request().getPathInContext());
 
-			if (retkey != null || CommonStartup.authflag == 0) {
-				if (CommonStartup.schema_Validatorflag > 0) {
+            if (m.find()) {
+                log.info("VES version:" + m.group());
+                vesVersion = m.group();
+            }
+            if (ctx.request().getPathInContext().contains("eventBatch")) {
+                CommonStartup.inlog.info(
+                    ctx.request().getRemoteAddress() + "VES Batch Input Messsage: " + jsonObject);
+                log.info(
+                    ctx.request().getRemoteAddress() + "VES Batch Input Messsage: " + jsonObject);
+                arrayFlag = 1;
+            } else {
+                CommonStartup.inlog
+                    .info(ctx.request().getRemoteAddress() + "Input Messsage: " + jsonObject);
+                log.info(ctx.request().getRemoteAddress() + "Input Messsage: " + jsonObject);
 
-					//fr = new FileReader(CommonStartup.schemaFile);
-					fr = new FileReader(schemaFileVersion(vesversion));
-					String schema = new JsonParser().parse(fr).toString();
+            }
 
-					valresult = CommonStartup.schemavalidate(jsonObject.toString(), schema);
-					if (valresult.equals("true")) {
-						log.info("Validation successful");
-					} else if (valresult.equals("false")) {
-						log.info("Validation failed");
-						respondWithCustomMsginJson(ctx, HttpStatusCodes.k400_badRequest, "Schema validation failed");
+            UUID uuid = UUID.randomUUID();
+            LoggingContext localLC = VESLogger.getLoggingContextForThread(uuid);
+            localLC.put(EcompFields.kBeginTimestampMs, SaClock.now());
 
-						return;
-					} else {
-						log.error("Validation errored" + valresult);
-						respondWithCustomMsginJson(ctx, HttpStatusCodes.k400_badRequest, "Couldn't parse JSON object");
-						return;
+            try {
+                if (CommonStartup.authflag == 1) {
+                    retkey = NsaBaseEndpoint.getAuthenticatedUser(ctx);
+                }
+            } catch (NullPointerException x) {
+                log.info(
+                    "Invalid user request " + ctx.request().getContentType() + MESSAGE
+                        + jsonObject);
+                CommonStartup.eplog.info("EVENT_RECEIPT_FAILURE: Unauthorized user" + x);
+                respondWithCustomMsginJson(ctx, HttpStatusCodes.k401_unauthorized, "Invalid user");
+                return;
+            }
 
-					}
-					
-					
-					if (arrayflag ==1) {
-						jsonArray = jsonObject.getJSONArray("eventList");
-						log.info("Validation successful for all events in batch");
-						for (int i = 0; i < jsonArray.length(); i++) {
-							event = new JSONObject().put("event", jsonArray.getJSONObject(i));
-							event.put("VESuniqueId", uuid + "-"+i);
-							event.put("VESversion", vesversion);
-							jsonArrayMod.put(event);
-						}
+            if (retkey != null || CommonStartup.authflag == 0) {
+                if (CommonStartup.schema_Validatorflag > 0) {
 
-						log.info("Modified jsonarray:" + jsonArrayMod.toString());
-						
-					}
-					else
-					{
-						
-						jsonObject.put("VESuniqueId", uuid);
-						jsonObject.put("VESversion", vesversion);
-						jsonArrayMod = new JSONArray().put(jsonObject);
-					}
-		
-				}
-				// reject anything that's not JSON
-				if (!ctx.request().getContentType().equalsIgnoreCase("application/json")) {
-					log.info("Rejecting request with content type " + ctx.request().getContentType() + " Message:"
-							+ jsonObject);
-					respondWithCustomMsginJson(ctx, HttpStatusCodes.k400_badRequest,
-							"Incorrect message content-type; only accepts application/json messages");
-					return;
-				}
+                    //fr = new FileReader(CommonStartup.schemaFile);
+                    fr = new FileReader(schemaFileVersion(vesVersion));
+                    String schema = new JsonParser().parse(fr).toString();
 
-				CommonStartup.handleEvents(jsonArrayMod);
-			} else {
-				log.info(
-						"Unauthorized request " + ctx.request().getContentType() + " Message:" + jsonObject.toString());
-				respondWithCustomMsginJson(ctx, HttpStatusCodes.k401_unauthorized, "Unauthorized user");
-				return;
-			}
-		} catch (JSONException | NullPointerException | IOException x) {
-			log.error("Couldn't parse JSON Array - HttpStatusCodes.k400_badRequest" + HttpStatusCodes.k400_badRequest
-					+ " Message:" + x.getMessage());
-			CommonStartup.eplog.info("EVENT_RECEIPT_FAILURE: Invalid user request " + x.toString());
-			respondWithCustomMsginJson(ctx, HttpStatusCodes.k400_badRequest, "Couldn't parse JSON object");
-			return;
-		} catch (QueueFullException e) {
-			e.printStackTrace();
-			log.error("Collector internal queue full  :" + e.getMessage());
-			CommonStartup.eplog.info("EVENT_RECEIPT_FAILURE: QueueFull" + e.toString());
-			respondWithCustomMsginJson(ctx, HttpStatusCodes.k503_serviceUnavailable, "Queue full");
-			return;
-		} finally {
-			if (fr != null) {
-				safeClose(fr);
-			}
+                    valresult = CommonStartup.schemavalidate(jsonObject.toString(), schema);
+                    if ("true".equals(valresult)) {
+                        log.info("Validation successful");
+                    } else if ("false".equals(valresult)) {
+                        log.info("Validation failed");
+                        respondWithCustomMsginJson(ctx, HttpStatusCodes.k400_badRequest,
+                            "Schema validation failed");
 
-			if (istr != null) {
-				safeClose(istr);
-			}
-		}
-		log.info("MessageAccepted and k200_ok to be sent");
-		ctx.response().sendErrorAndBody(HttpStatusCodes.k200_ok, "Message Accepted", MimeTypes.kAppJson);
-	}
+                        return;
+                    } else {
+                        log.error("Validation errored" + valresult);
+                        respondWithCustomMsginJson(ctx, HttpStatusCodes.k400_badRequest,
+                            "Couldn't parse JSON object");
+                        return;
 
-	
-	public static void respondWithCustomMsginJson(DrumlinRequestContext ctx, int sc, String msg) {
-		String[] str = null;
-		String ExceptionType = "GeneralException";
+                    }
 
-		str = CustomExceptionLoader.LookupMap(String.valueOf(sc), msg);
-		System.out.println("Post CustomExceptionLoader.LookupMap" + str);
+                    if (arrayFlag == 1) {
+                        jsonArray = jsonObject.getJSONArray("eventList");
+                        log.info("Validation successful for all events in batch");
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            event = new JSONObject().put("event", jsonArray.getJSONObject(i));
+                            event.put("VESuniqueId", uuid + "-" + i);
+                            event.put("VESversion", vesVersion);
+                            jsonArrayMod.put(event);
+                        }
 
-		if (str != null) {
+                        log.info("Modified jsonarray:" + jsonArrayMod);
 
-			if (str[0].matches("SVC")) {
-				ExceptionType = "ServiceException";
-			} else if (str[1].matches("POL")) {
-				ExceptionType = "PolicyException";
-			}
+                    } else {
 
-			JSONObject jb = new JSONObject().put("requestError",
-					new JSONObject().put(ExceptionType, new JSONObject().put("MessagID", str[0]).put("text", str[1])));
+                        jsonObject.put("VESuniqueId", uuid);
+                        jsonObject.put("VESversion", vesVersion);
+                        jsonArrayMod = new JSONArray().put(jsonObject);
+                    }
 
-			log.debug("Constructed json error : " + jb.toString());
-			ctx.response().sendErrorAndBody(sc, jb.toString(), MimeTypes.kAppJson);
-		} else {
-			JSONObject jb = new JSONObject().put("requestError",
-					new JSONObject().put(ExceptionType, new JSONObject().put("Status", sc).put("Error", msg)));
-			ctx.response().sendErrorAndBody(sc, jb.toString(), MimeTypes.kAppJson);
-		}
+                }
+                // reject anything that's not JSON
+                if (!ctx.request().getContentType().equalsIgnoreCase("application/json")) {
+                    log.info(String.format("Rejecting request with content type %s Message:%s",
+                        ctx.request().getContentType(), jsonObject));
+                    respondWithCustomMsginJson(ctx, HttpStatusCodes.k400_badRequest,
+                        "Incorrect message content-type; only accepts application/json messages");
+                    return;
+                }
 
-	}
+                CommonStartup.handleEvents(jsonArrayMod);
+            } else {
+                log.info(
+                    String.format("Unauthorized request %s%s%s", ctx.request().getContentType(),
+                        MESSAGE, jsonObject));
+                respondWithCustomMsginJson(ctx, HttpStatusCodes.k401_unauthorized,
+                    "Unauthorized user");
+                return;
+            }
+        } catch (JSONException | NullPointerException | IOException x) {
+            log.error(String
+                .format("Couldn't parse JSON Array - HttpStatusCodes.k400_badRequest%d%s%s",
+                    HttpStatusCodes.k400_badRequest, MESSAGE, x.getMessage()));
+            CommonStartup.eplog.info("EVENT_RECEIPT_FAILURE: Invalid user request " + x);
+            respondWithCustomMsginJson(ctx, HttpStatusCodes.k400_badRequest,
+                "Couldn't parse JSON object");
+            return;
+        } catch (QueueFullException e) {
+            log.error("Collector internal queue full  :" + e.getMessage(), e);
+            CommonStartup.eplog.info("EVENT_RECEIPT_FAILURE: QueueFull" + e);
+            respondWithCustomMsginJson(ctx, HttpStatusCodes.k503_serviceUnavailable, "Queue full");
+            return;
+        } finally {
+            if (fr != null) {
+                safeClose(fr);
+            }
 
-	public static void safeClose(FileReader fr) {
-		if (fr != null) {
-			try {
-				fr.close();
-			} catch (IOException e) {
-				log.error("Error closing file reader stream : " + e.toString());
-			}
-		}
+            if (istr != null) {
+                safeClose(istr);
+            }
+        }
+        log.info("MessageAccepted and k200_ok to be sent");
+        ctx.response()
+            .sendErrorAndBody(HttpStatusCodes.k200_ok, "Message Accepted", MimeTypes.kAppJson);
+    }
 
-	}
 
-	public static void safeClose(InputStream is) {
-		if (is != null) {
-			try {
-				is.close();
-			} catch (IOException e) {
-				log.error("Error closing Input stream : " + e.toString());
-			}
-		}
+    public static void respondWithCustomMsginJson(DrumlinRequestContext ctx, int sc, String msg) {
+        String[] str;
+        String exceptionType = "GeneralException";
 
-	}
+        str = CustomExceptionLoader.LookupMap(String.valueOf(sc), msg);
+        log.info("Post CustomExceptionLoader.LookupMap" + str);
 
-	public static String schemaFileVersion(String version)
-	{
-		String filename = null;
-		
-		if (CommonStartup.schemaFileJson.has(version))
-		{
-			filename = CommonStartup.schemaFileJson.getString(version);
-		}
-		else
-		{
-			filename = CommonStartup.schemaFile;
-		}
-		log.info("VESversion: " + version + " Schema File:" + filename);
-		return filename;
-		
-	}
-	
+        if (str != null) {
+
+            if (str[0].matches("SVC")) {
+                exceptionType = "ServiceException";
+            } else if (str[1].matches("POL")) {
+                exceptionType = "PolicyException";
+            }
+
+            JSONObject jb = new JSONObject().put("requestError",
+                new JSONObject().put(exceptionType,
+                    new JSONObject().put("MessagID", str[0]).put("text", str[1])));
+
+            log.debug("Constructed json error : " + jb);
+            ctx.response().sendErrorAndBody(sc, jb.toString(), MimeTypes.kAppJson);
+        } else {
+            JSONObject jb = new JSONObject().put("requestError",
+                new JSONObject()
+                    .put(exceptionType, new JSONObject().put("Status", sc).put("Error", msg)));
+            ctx.response().sendErrorAndBody(sc, jb.toString(), MimeTypes.kAppJson);
+        }
+
+    }
+
+    public static void safeClose(FileReader fr) {
+        if (fr != null) {
+            try {
+                fr.close();
+            } catch (IOException e) {
+                log.error("Error closing file reader stream : " + e);
+            }
+        }
+
+    }
+
+    public static void safeClose(InputStream is) {
+        if (is != null) {
+            try {
+                is.close();
+            } catch (IOException e) {
+                log.error("Error closing Input stream : " + e);
+            }
+        }
+
+    }
+
+    public static String schemaFileVersion(String version) {
+        String filename = null;
+
+        if (CommonStartup.schemaFileJson.has(version)) {
+            filename = CommonStartup.schemaFileJson.getString(version);
+        } else {
+            filename = CommonStartup.schemaFile;
+        }
+        log.info(String.format("VESversion: %s Schema File:%s", version, filename));
+        return filename;
+
+    }
 
 }
