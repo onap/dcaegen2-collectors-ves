a38b4cf499b4461d2dcf747837bd346b1b2c71a5
Bharat saraswal <bharat.saraswal@huawei.com>
2017-09-17 13:49:51 +0530

    Fixing sonar and javadoc issues.
    minor code refactoring.
    
    Issue-Id:DCAEGEN2-92
    
    Change-Id: I9eef305391d09f92abf8beb58be0c8ee343b2fbb
    Signed-off-by: Bharat saraswal <bharat.saraswal@huawei.com>

--- src/main/java/org/onap/dcae/controller/FetchDynamicConfig.java
+++ src/main/java/org/onap/dcae/controller/FetchDynamicConfig.java
@@ -20,95 +20,99 @@
 
 package org.onap.dcae.controller;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Map;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.json.JSONTokener;
-
 public class FetchDynamicConfig {
 
-	static String configFile = "/opt/app/KV-Configuration.json";
-	static String url = null;
-	static String ret_string = null;
+    private static final Logger log = LoggerFactory.getLogger(FetchDynamicConfig.class);
 
-	public FetchDynamicConfig() {
+    static String configFile = "/opt/app/KV-Configuration.json";
+    static String url;
+    static String retString;
 
-	}
+    public FetchDynamicConfig() {
+    }
 
-	public static void main(String[] args) {
-		Map<String, String> env = System.getenv();
-		for (String envName : env.keySet()) {
-			System.out.format("%s=%s%n", envName, env.get(envName));
-		}
+    public static void main(String[] args) {
+        Map<String, String> env = System.getenv();
+        for (Map.Entry<String, String> entry : env.entrySet()) {
+            log.info("%s=%s%n", entry.getKey(), entry.getValue());
+        }
 
-		if (env.containsKey("CONSUL_HOST") && env.containsKey("CONFIG_BINDING_SERVICE")
-				 && env.containsKey("HOSTNAME")) {
-			System.out.println(">>>Dynamic configuration to be fetched from ConfigBindingService");
-			url = env.get("CONSUL_HOST") + ":8500/v1/catalog/service/" + env.get("CONFIG_BINDING_SERVICE");
+        if (env.containsKey("CONSUL_HOST") && env.containsKey("CONFIG_BINDING_SERVICE")
+            && env.containsKey("HOSTNAME")) {
+            log.info(">>>Dynamic configuration to be fetched from ConfigBindingService");
+            url = env.get("CONSUL_HOST") + ":8500/v1/catalog/service/" + env
+                .get("CONFIG_BINDING_SERVICE");
 
-			ret_string = executecurl(url);
-			// consul return as array
-			JSONTokener temp = new JSONTokener(ret_string);
-			JSONObject cbsjobj = (JSONObject) new JSONArray(temp).get(0);
+            retString = executecurl(url);
+            // consul return as array
+            JSONTokener temp = new JSONTokener(retString);
+            JSONObject cbsjobj = (JSONObject) new JSONArray(temp).get(0);
 
-			String url_part1 = null;
-			if (cbsjobj.has("ServiceAddress") && cbsjobj.has("ServicePort")) {
-				url_part1 = cbsjobj.getString("ServiceAddress") + ":" + cbsjobj.getInt("ServicePort");
-			}
+            String urlPart1 = null;
+            if (cbsjobj.has("ServiceAddress") && cbsjobj.has("ServicePort")) {
+                urlPart1 =
+                    cbsjobj.getString("ServiceAddress") + ":" + cbsjobj.getInt("ServicePort");
+            }
 
-			System.out.println("CONFIG_BINDING_SERVICE DNS RESOLVED:" + url_part1);
-			url = url_part1 + "/service_component/" + env.get("HOSTNAME");
-			ret_string = executecurl(url);
+            log.info("CONFIG_BINDING_SERVICE DNS RESOLVED:" + urlPart1);
+            url = urlPart1 + "/service_component/" + env.get("HOSTNAME");
+            retString = executecurl(url);
 
-			JSONObject jsonObject = new JSONObject(new JSONTokener(ret_string));
-			try (FileWriter file = new FileWriter(configFile)) {
-				file.write(jsonObject.toString());
+            JSONObject jsonObject = new JSONObject(new JSONTokener(retString));
+            try (FileWriter file = new FileWriter(configFile)) {
+                file.write(jsonObject.toString());
 
-				System.out.println("Successfully Copied JSON Object to file /opt/app/KV-Configuration.json");
-			} catch (IOException e) {
-				System.out.println(
-						"Error in writing configuration into file /opt/app/KV-Configuration.json " + jsonObject);
-				e.printStackTrace();
-			}
-		}
+                log.info(
+                    "Successfully Copied JSON Object to file /opt/app/KV-Configuration.json");
+            } catch (IOException e) {
+                log.error(
+                    "Error in writing configuration into file /opt/app/KV-Configuration.json "
+                        + jsonObject, e);
+            }
+        } else {
+            log.info(">>>Static configuration to be used");
+        }
 
-		else {
-			System.out.println(">>>Static configuration to be used");
-		}
+    }
 
-	}
+    public static String executecurl(String url) {
 
-	public static String executecurl(String url) {
+        String[] command = {"curl", "-v", url};
+        ProcessBuilder process = new ProcessBuilder(command);
+        Process p;
+        String result = null;
+        try {
+            p = process.start();
+            InputStreamReader ipr = new InputStreamReader(p.getInputStream());
+            BufferedReader reader = new BufferedReader(ipr);
+            StringBuilder builder = new StringBuilder();
+            String line;
 
-		String[] command = { "curl", "-v", url };
-		ProcessBuilder process = new ProcessBuilder(command);
-		Process p;
-		String result = null;
-		try {
-			p = process.start();
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+            result = builder.toString();
+            log.info(result);
 
-			InputStreamReader ipr = new InputStreamReader(p.getInputStream());
-			BufferedReader reader = new BufferedReader(ipr);
-			StringBuilder builder = new StringBuilder();
-			String line = null;
+            reader.close();
+            ipr.close();
+        } catch (IOException e) {
+            log.error("error", e);
+        }
+        return result;
 
-			while ((line = reader.readLine()) != null) {
-				builder.append(line);
-			}
-			result = builder.toString();
-			System.out.println(result);
-
-		} catch (IOException e) {
-			System.out.print("error");
-			e.printStackTrace();
-		}
-		return result;
-
-	}
+    }
 
 }
--- src/main/java/org/onap/dcae/controller/LoadDynamicConfig.java
+++ src/main/java/org/onap/dcae/controller/LoadDynamicConfig.java
@@ -20,6 +20,12 @@
 
 package org.onap.dcae.controller;
 
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -28,131 +34,118 @@ import java.io.InputStreamReader;
 import java.util.Iterator;
 import java.util.Map;
 
-import org.apache.commons.configuration.ConfigurationException;
-import org.apache.commons.configuration.PropertiesConfiguration;
-
-import org.json.JSONObject;
-
- 
 
 public class LoadDynamicConfig {
 
-	static String propFile = "collector.properties";
-	static String configFile = "/opt/app/KV-Configuration.json";
-	static String url = null;
-	static String ret_string = null;
+    private static final Logger log = LoggerFactory.getLogger(LoadDynamicConfig.class);
 
-	public LoadDynamicConfig() {
+    static String propFile = "collector.properties";
+    static String configFile = "/opt/app/KV-Configuration.json";
+    static String url;
+    static String retString;
 
-	}
+    public LoadDynamicConfig() {
 
-	public static void main(String[] args) {
-		Map<String, String> env = System.getenv();
-		/*for (String envName : env.keySet()) {
-			System.out.format("%s=%s%n", envName, env.get(envName));
+    }
+
+    public static void main(String[] args) {
+        Map<String, String> env = System.getenv();
+        /*for (String envName : env.keySet()) {
+            System.out.format("%s=%s%n", envName, env.get(envName));
 		}*/
 
-		//Check again to ensure new controller deployment related config
-		if (env.containsKey("CONSUL_HOST") &&
-		 env.containsKey("CONFIG_BINDING_SERVICE")  && env.containsKey("HOSTNAME"))
-		{
-		
-			
-			try {
+        //Check again to ensure new controller deployment related config
+        if (env.containsKey("CONSUL_HOST") &&
+            env.containsKey("CONFIG_BINDING_SERVICE") && env.containsKey("HOSTNAME")) {
 
-			 	String jsonData = readFile(configFile);
-			 	JSONObject jsonObject = new JSONObject(jsonData);
-			 	
-				PropertiesConfiguration conf;
-				conf = new PropertiesConfiguration(propFile);
-				conf.setEncoding(null);
+            try {
 
-				
-				if (jsonObject != null) {
-					// update properties based on consul dynamic configuration
-					Iterator<?> keys = jsonObject.keys();
+                String jsonData = readFile(configFile);
+                JSONObject jsonObject = new JSONObject(jsonData);
 
-					while (keys.hasNext()) {
-						String key = (String) keys.next();
-						// check if any configuration is related to dmaap
-						// and write into dmaapconfig.json
-						if (key.startsWith("streams_publishes")) {
-							//VESCollector only have publish streams
-							try (FileWriter file = new FileWriter("./etc/DmaapConfig.json")) {
-								file.write(jsonObject.get(key).toString());
-								System.out.println("Successfully written JSON Object to DmaapConfig.json");
-							} catch (IOException e) {
-								System.out.println("Error in writing dmaap configuration into DmaapConfig.json");
-								e.printStackTrace();
-							}
-						} else {
-							conf.setProperty(key, jsonObject.get(key).toString());
-						}
+                PropertiesConfiguration conf;
+                conf = new PropertiesConfiguration(propFile);
+                conf.setEncoding(null);
 
-					}
-					conf.save();
+                // update properties based on consul dynamic configuration
+                Iterator<?> keys = jsonObject.keys();
 
+                while (keys.hasNext()) {
+                    String key = (String) keys.next();
+                    // check if any configuration is related to dmaap
+                    // and write into dmaapconfig.json
+                    if (key.startsWith("streams_publishes")) {
+                        //VESCollector only have publish streams
+                        try (FileWriter file = new FileWriter("./etc/DmaapConfig.json")) {
+                            file.write(jsonObject.get(key).toString());
+                            log.info("Successfully written JSON Object to DmaapConfig.json");
+                            file.close();
+                        } catch (IOException e) {
+                            log.info(
+                                "Error in writing dmaap configuration into DmaapConfig.json",
+                                e);
+                        }
+                    } else {
+                        conf.setProperty(key, jsonObject.get(key).toString());
+                    }
 
-				}
-			} catch (ConfigurationException e) {
-				e.getMessage();
-				e.printStackTrace();
+                }
+                conf.save();
 
-			}
-			
-		}
-		else
-		{
-			System.out.println(">>>Static configuration to be used");
-		}
+            } catch (ConfigurationException e) {
+                log.error(e.getLocalizedMessage(), e);
 
-	}
+            }
 
-	public static String executecurl(String url) {
+        } else {
+            log.info(">>>Static configuration to be used");
+        }
 
-		String[] command = { "curl", "-v", url };
-		ProcessBuilder process = new ProcessBuilder(command);
-		Process p;
-		String result = null;
-		try {
-			p = process.start();
+    }
 
-			InputStreamReader ipr= new InputStreamReader(p.getInputStream());
-			BufferedReader reader = new BufferedReader(ipr);
-			StringBuilder builder = new StringBuilder();
-			String line = null;
-			
-			while ((line = reader.readLine()) != null) {
-				builder.append(line);
-			}
-			result = builder.toString();
-			System.out.println(result);
-			
-		} catch (IOException e) {
-			System.out.print("error");
-			e.printStackTrace();
-		}
-		return result;
+    public static String executecurl(String url) {
 
-	}
+        String[] command = {"curl", "-v", url};
+        ProcessBuilder process = new ProcessBuilder(command);
+        Process p;
+        String result = null;
+        try {
+            p = process.start();
 
-	public static String readFile(String filename) {
-	    String result = "";
-	    try {
-	        BufferedReader br = new BufferedReader(new FileReader(filename));
-	        StringBuilder sb = new StringBuilder();
-	        String line = br.readLine();
-	        while (line != null) {
-	            sb.append(line);
-	            line = br.readLine();
-	        }
-	        result = sb.toString();
-	        br.close();
-	    } catch(Exception e) {
-	        e.printStackTrace();
-	    }
-	    return result;
-	}
+            InputStreamReader ipr = new InputStreamReader(p.getInputStream());
+            BufferedReader reader = new BufferedReader(ipr);
+            StringBuilder builder = new StringBuilder();
+            String line = null;
+
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+            result = builder.toString();
+            log.info(result);
+
+        } catch (IOException e) {
+            log.error("error" + e.getLocalizedMessage(), e);
+        }
+        return result;
+
+    }
+
+    public static String readFile(String filename) {
+        String result = "";
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+            while (line != null) {
+                sb.append(line);
+                line = br.readLine();
+            }
+            result = sb.toString();
+            br.close();
+        } catch (Exception e) {
+            log.error(e.getLocalizedMessage(), e);
+        }
+        return result;
+    }
 
 
 }
