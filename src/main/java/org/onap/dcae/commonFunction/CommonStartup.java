c892293c6325051828f58c510d79a9dc19363edb
Konstantinos Kanonakis <konstantinos.kanonakis@huawei.com>
2017-10-06 11:13:59 -0500

    Refactoring DMaaP config reading
    - Introducing new DMaaPChannel/DMaaPChannels classes
     to hold DMaaP channel configuration
    - There are two ways to read config:
    	> List of DMaaPChannel can be parsed directly from
    	legacy-formatted JSON file using Jackson
    	> DMaaPChannel instances can be created with info parsed
    	using Regex from URLs found in config file following new format
    - Removing previously used HashMap and introducing new one
    that allows retrieving DMaaPChannel objects from their name
    - Refactoring EventPublisher to use DMaaPChannel instance
    obtained from DMaapPropertyReader channel map
    - Rewriting DmaapPropertyReader unit test to use DMaaPChannel
    and including tests for new config format
    
    Issue-Id: DCAEGEN2-129
    Change-Id: I34c9014b597580fa03bb69e3234ef5dfe0d046e6
    Signed-off-by: Konstantinos Kanonakis <konstantinos.kanonakis@huawei.com>

--- src/main/java/org/onap/dcae/commonFunction/CommonStartup.java
+++ src/main/java/org/onap/dcae/commonFunction/CommonStartup.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -295,7 +295,7 @@ public class CommonStartup extends NsaBaseEndpoint implements Runnable {
 			result = String.valueOf(report.isSuccess());
 		}
 		try {
-			log.debug("Validation Result:" + result + " Validation report:" + report);
+			log.debug("Validation Result:" + result + " Validation report:" + report.isSuccess());
 		} catch (NullPointerException e) {
 			log.error("schemavalidate:NullpointerException on report");
 		}
