01168c9013e7355b388d44b6a8f3f8284d63f70d
PawelSzalapski <pawel.szalapski@nokia.com>
2018-06-06 15:30:06 +0200

    Refactor tests to check actual behavior of methods
    Testcases were skimming through the code, but actually make no asserts.
    
    Change-Id: Icba8fe890c01fc11a4507ca468ab4d9b6141caf9
    Issue-ID: DCAEGEN2-524
    Signed-off-by: PawelSzalapski <pawel.szalapski@nokia.com>

--- src/test/java/org/onap/dcae/vestest/TestCommonStartup.java
+++ src/test/java/org/onap/dcae/vestest/TestCommonStartup.java
@@ -19,154 +19,117 @@
  */
 package org.onap.dcae.vestest;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.net.URL;
-import java.util.Map;
-import java.util.concurrent.LinkedBlockingQueue;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.onap.dcae.commonFunction.CommonStartup;
-import org.onap.dcae.commonFunction.EventProcessor;
-import org.onap.dcae.commonFunction.CommonStartup.QueueFullException;
-import org.onap.dcae.restapi.RestfulCollectorServlet;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 import com.att.nsa.cmdLine.NsaCommandLineUtil;
 import com.att.nsa.drumlin.service.framework.DrumlinServlet;
-import com.att.nsa.drumlin.till.nv.rrNvReadable.loadException;
-import com.att.nsa.drumlin.till.nv.rrNvReadable.missingReqdSetting;
-import com.att.nsa.security.NsaAuthenticator;
-import com.att.nsa.security.authenticators.SimpleAuthenticator;
-import com.att.nsa.security.db.simple.NsaSimpleApiKey;
-import com.google.gson.JsonParser;
+import com.att.nsa.drumlin.service.framework.context.DrumlinRequest;
 import com.att.nsa.drumlin.till.nv.impl.nvPropertiesFile;
 import com.att.nsa.drumlin.till.nv.impl.nvReadableStack;
 import com.att.nsa.drumlin.till.nv.impl.nvReadableTable;
+import com.att.nsa.drumlin.till.nv.rrNvReadable.loadException;
+import com.att.nsa.drumlin.till.nv.rrNvReadable.missingReqdSetting;
+import com.att.nsa.security.authenticators.SimpleAuthenticator;
+import com.att.nsa.security.db.simple.NsaSimpleApiKey;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Base64;
+import java.util.Map;
+import java.util.concurrent.LinkedBlockingQueue;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.onap.dcae.commonFunction.CommonStartup;
+import org.onap.dcae.commonFunction.CommonStartup.QueueFullException;
+import org.onap.dcae.commonFunction.EventProcessor;
+import org.onap.dcae.restapi.RestfulCollectorServlet;
 
 
 public class TestCommonStartup {
 
-        String payload = null;
-        @Before
-        public void setUp() throws Exception {
+    @Test
+    public void shouldParseCLIArguments() {
+        // given
+        String args[] = {"-a", "aa"};
+        Map<String, String> argMap = NsaCommandLineUtil.processCmdLine(args, true);
+        // when
+        nvReadableStack settings = new nvReadableStack();
+        settings.push(new nvReadableTable(argMap));
 
-                // process command line arguments
-                payload = new JsonParser().parse(new FileReader("src/test/resources/VES_valid.txt")).toString();
-                CommonStartup.fProcessingInputQueue = new LinkedBlockingQueue<JSONObject> (CommonStartup.KDEFAULT_MAXQUEUEDEVENTS);
-        }
+        // then
+        assertEquals(settings.getString("a", "default"), "aa");
+    }
 
-        @After
-        public void tearDown() throws Exception {
+    @Test
+    public void shouldPutValidVESEventOnProcessingQueueWithoutExceptions() throws IOException, QueueFullException {
+        // given
+        CommonStartup.fProcessingInputQueue = new LinkedBlockingQueue<>(
+            CommonStartup.KDEFAULT_MAXQUEUEDEVENTS);
+        JsonElement vesEvent = new JsonParser().parse(new FileReader("src/test/resources/VES_valid.txt"));
+        JSONObject validVESEvent = new JSONObject(vesEvent.toString());
+        JSONArray jsonArrayMod = new JSONArray().put(validVESEvent);
 
-        }
-
-        @Test
-        public void testCommonStartupload() {
-
-                String args[] = { "junittest" };
-                final Map<String, String> argMap = NsaCommandLineUtil.processCmdLine(args, true);
-                final String config = NsaCommandLineUtil.getSetting(argMap, "c", "collector.properties");
-                final URL settingStream = DrumlinServlet.findStream(config, CommonStartup.class);
-
-                final nvReadableStack settings = new nvReadableStack();
-                try {
-                        settings.push(new nvPropertiesFile(settingStream));
-                } catch (loadException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                }
-                settings.push(new nvReadableTable(argMap));
-                Assert.assertEquals("true", "true");
-        }
-
-        @Test
-        public void testhandleevent() {
-                JSONArray jsonArrayMod = new JSONArray().put(new JSONObject(payload));
-                try {
+        // then
+        CommonStartup.handleEvents(jsonArrayMod);
+    }
 
 
-                        CommonStartup.handleEvents (jsonArrayMod);
-                } catch ( JSONException | QueueFullException | IOException e) {
-                        // TODO Auto-generated catch block
-                        //e.printStackTrace();
-                        System.out.println("junit reported:" + e.getMessage());
-                }
-                Assert.assertEquals("true", "true");
-        }
+    @Test
+    public void shouldParseStreamIdToStreamHashMapping() {
+        // given
+        CommonStartup.streamid = "fault=sec_fault|syslog=sec_syslog|heartbeat=sec_heartbeat|measurementsForVfScaling=sec_measurement|mobileFlow=sec_mobileflow|other=sec_other|stateChange=sec_statechange|thresholdCrossingAlert=sec_thresholdCrossingAlert|voiceQuality=ves_voicequality|sipSignaling=ves_sipsignaling";
+        EventProcessor eventProcessor = new EventProcessor();
 
+        // when
+        Map<String, String[]> streamHashMapping = eventProcessor
+            .parseStreamIdToStreamHashMapping(CommonStartup.streamid);
 
-/*
-        @Test
-        public void testServlet()  {
-                try
-                {
-                        RestfulCollectorServlet rsv = new RestfulCollectorServlet(null);
-                }
-                catch (NullPointerException|loadException| missingReqdSetting e){
-                        System.out.println("junit reported:" + e.getMessage());
-                }
-                Assert.assertEquals("true", "true");
-        }
-*/
+        // then
+        assertEquals(streamHashMapping.get("fault")[0], "sec_fault");
+        assertEquals(streamHashMapping.get("measurementsForVfScaling")[0], "sec_measurement");
+    }
 
+    @Test
+    public void testAuthListHandler() throws loadException, missingReqdSetting {
+        // given
+        final nvReadableStack settings = new nvReadableStack();
 
-     
-		@Test
-        public void testEventProcessorinstantiation()
-        {
-                CommonStartup.streamid="fault=sec_fault|syslog=sec_syslog|heartbeat=sec_heartbeat|measurementsForVfScaling=sec_measurement|mobileFlow=sec_mobileflow|other=sec_other|stateChange=sec_statechange|thresholdCrossingAlert=sec_thresholdCrossingAlert|voiceQuality=ves_voicequality|sipSignaling=ves_sipsignaling";
-                EventProcessor ep = new EventProcessor ();
-                Thread epThread=new Thread(ep);
-                epThread.start();
-                 Assert.assertEquals("true", "true");
-                epThread.interrupt();
+        String user1 = "secureid";
+        String password1Hashed = "IWRjYWVSb2FkbTEyMyEt";
+        String password1UnHashed = decode("IWRjYWVSb2FkbTEyMyEt");
+        String user2 = "sample1";
+        String password2Hashed = "c2FtcGxlMQ";
 
-        }
+        String authlist = user1 + "," + password1Hashed + "|" + user2 + "," + password2Hashed;
 
-        @Test
-        public void testAuthListHandler()
-        {
-        	
-        	final Map<String, String> argMap = NsaCommandLineUtil.processCmdLine ( new String[0], true );
-			final String config = NsaCommandLineUtil.getSetting ( argMap, "c", "collector.properties" );
-			final URL settingStream = DrumlinServlet.findStream ( config, CommonStartup.class );
+        RestfulCollectorServlet rsv = new RestfulCollectorServlet(settings);
 
-			final nvReadableStack settings = new nvReadableStack ();
-			try {
-				settings.push ( new nvPropertiesFile ( settingStream ) );
-				settings.push ( new nvReadableTable ( argMap ) );
-			} catch (loadException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			
-			
-        	RestfulCollectorServlet rsv = null;
-        	NsaAuthenticator<NsaSimpleApiKey> NsaAuth = null;
-        	Boolean flag = false;
-	            try
-	            {
-	                    rsv = new RestfulCollectorServlet(settings);
-	            }
-	            catch (NullPointerException|loadException| missingReqdSetting e){
-	                    System.out.println("junit reported:" + e.getMessage());
-	            }
-	            String authlist = "secureid,IWRjYWVSb2FkbTEyMyEt|sample1,c2FtcGxlMQ==|vdnsagg,dmRuc2FnZw==";
-	            NsaAuth = rsv.AuthlistHandler(authlist);
-	            if (NsaAuth != null)
-	            {
-	            	flag = true;
-	            }
-                Assert.assertEquals(true, flag);
-              
+        DrumlinRequest drumlinRequestMock = Mockito.mock(DrumlinRequest.class);
 
-        }
+        String basicHeaderForUser1 = "Basic " + encode(user1, password1UnHashed);
+        when(drumlinRequestMock.getFirstHeader("Authorization")).thenReturn(basicHeaderForUser1);
+
+        // when
+        SimpleAuthenticator simpleAuthenticator = (SimpleAuthenticator) rsv.AuthlistHandler(authlist);
+        NsaSimpleApiKey authentic = simpleAuthenticator.isAuthentic(drumlinRequestMock);
+
+        // then
+        assertEquals(authentic.getSecret(), password1UnHashed);
+    }
+
+    private String decode(String hashedPassword) {
+        return new String(Base64.getDecoder().decode(hashedPassword.getBytes()));
+    }
+
+    private String encode(String user1, String password1UnHashed) {
+        return Base64.getEncoder().encodeToString((user1 + ":" + password1UnHashed).getBytes());
+    }
+
 }
 
 
