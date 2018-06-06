7463e9fab9d03f96253292d71db8f372ed98c2ac
PawelSzalapski <pawel.szalapski@nokia.com>
2018-06-06 14:55:12 +0200

    Refactor DMaaPPropertyReader tests
    DMaaPPropertyReader tests had no meaningful assert statements
    Tests for actual topic retrieval were added
    
    Change-Id: Ieb861f02d8484ac4bfd5a49b3c70d296bd2c129b
    Issue-ID: DCAEGEN2-524
    Signed-off-by: PawelSzalapski <pawel.szalapski@nokia.com>

--- src/test/java/org/onap/dcae/vestest/TestDmaapPropertyReader.java
+++ src/test/java/org/onap/dcae/vestest/TestDmaapPropertyReader.java
@@ -1,15 +1,16 @@
 /*-
  * ============LICENSE_START=======================================================
- * PROJECT
+ * org.onap.dcaegen2.collectors.ves
  * ================================================================================
  * Copyright (C) 2017-2018 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2018 Nokia. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,49 +20,34 @@
  */
 package org.onap.dcae.vestest;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
+import java.util.HashMap;
+import org.junit.Test;
 import org.onap.dcae.commonFunction.DmaapPropertyReader;
 
 public class TestDmaapPropertyReader {
 
-	DmaapPropertyReader dr;
+    @Test
+    public void shouldReadDMaaPHashes() {
+        DmaapPropertyReader dmaapPropertyReader = new DmaapPropertyReader("src/test/resources/testDmaapConfig_ip.json");
+        HashMap<String, String> dmaapHash = dmaapPropertyReader.dmaap_hash;
+        assertEquals(dmaapHash.get("sec_fault_ueb.cambria.hosts"),
+            "uebsb91kcdc.it.att.com,uebsb92kcdc.it.att.com,uebsb93kcdc.it.att.com");
+        assertEquals(dmaapHash.get("sec_fault_ueb.cambria.topic"), "DCAE-SE-COLLECTOR-EVENTS-DEV");
+    }
 
-	Boolean flag = false;
+    @Test
+    public void shouldReadDMaaPHashesForSecondGeneration() {
+        DmaapPropertyReader dmaapPropertyReader = new DmaapPropertyReader(
+            "src/test/resources/testDmaapConfig_gen2.json");
+        HashMap<String, String> dmaapHash = dmaapPropertyReader.dmaap_hash;
+        assertEquals(dmaapHash.get("ves-thresholdCrossingAlert-secondary.cambria.topic"),
+            "DCAE-SE-COLLECTOR-EVENTS-DEV");
+        assertEquals(dmaapHash.get("ves-thresholdCrossingAlert-secondary.cambria.url"), "UEBHOST:3904");
+        assertEquals(dmaapHash.get("ves-fault-secondary.cambria.url"), "UEBHOST:3904");
+        assertEquals(dmaapHash.get("ves-fault-secondary.cambria.topic"), "DCAE-SE-COLLECTOR-EVENTS-DEV");
+    }
 
-	@Before
-	public void setUp() throws Exception {
-
-		// process command line arguments
-
-		
-
-	}
-
-	@After
-	public void tearDown() throws Exception {
-
-	}
-
-	@Test
-	public void testDmaapPropertyReader() {
-		String testinput = "src/test/resources/testDmaapConfig_ip.json";
-		dr = new DmaapPropertyReader(testinput);
-		Boolean test = true;
-		flag = !dr.dmaap_hash.isEmpty();
-		Assert.assertEquals(test, flag);
-	}
-
-	@Test
-	public void testDmaapPropertyReaderNewConfig() {
-		String testinput = "src/test/resources/testDmaapConfig_gen2.json";
-		dr = new DmaapPropertyReader(testinput);
-		Boolean test = true;
-		flag = !dr.dmaap_hash.isEmpty();
-		Assert.assertEquals(test, flag);
-	}
 }
 
