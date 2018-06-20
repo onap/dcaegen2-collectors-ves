dc79909fc23d3dbfe573c420c18a895aa81bd029
PawelSzalapski <pawel.szalapski@nokia.com>
2018-06-20 15:29:46 +0200

    Prepare codebase for dynamic DMaaP configuration
    From now on, there is only one single place
    where we can create whole app core concerning sending events
    and it has a single entry point, based on DMaaP file path.
    It can be used to rebuild this part of app dynamically.
    Changes are in scope for the dynamic DMaaP config feature.
    + bumped up code coverage a bit
    
    Signed-off-by: PawelSzalapski <pawel.szalapski@nokia.com>
    Issue-ID: DCAEGEN2-517
    Change-Id: I9978a76d3fc74b745096d0eb47c34055dfb0e861

---
+++ src/main/java/org/onap/dcae/commonFunction/event/publishing/CambriaPublishersCache.java
@@ -0,0 +1,136 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * org.onap.dcaegen2.collectors.ves
+ * ================================================================================
+ * Copyright (C) 2017-2018 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2018 Nokia. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.dcae.commonFunction.event.publishing;
+
+import static io.vavr.API.Option;
+import static io.vavr.API.unchecked;
+import static java.lang.String.format;
+
+import com.att.nsa.cambria.client.CambriaBatchingPublisher;
+import com.att.nsa.cambria.client.CambriaClientBuilders;
+import com.att.nsa.cambria.client.CambriaClientBuilders.PublisherBuilder;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.cache.RemovalListener;
+import com.google.common.cache.RemovalNotification;
+import io.vavr.collection.Map;
+import io.vavr.control.Option;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import javax.annotation.Nonnull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class CambriaPublishersCache {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CambriaPublishersCache.class);
+    private final LoadingCache<String, CambriaBatchingPublisher> publishersCache;
+
+    @VisibleForTesting
+    CambriaPublishersCache(CambriaPublishersCacheLoader cambriaPublishersCacheLoader) {
+        this.publishersCache = CacheBuilder.newBuilder()
+            .removalListener(new OnPublisherRemovalListener())
+            .build(cambriaPublishersCacheLoader);
+    }
+
+    static CambriaPublishersCache create(Map<String, PublisherConfig> dMaaPPublisherConfigs) {
+        return new CambriaPublishersCache(new CambriaPublishersCacheLoader(dMaaPPublisherConfigs));
+    }
+
+    Option<CambriaBatchingPublisher> getPublisher(String streamID) {
+        try {
+            return Option(publishersCache.getUnchecked(streamID));
+        } catch (Exception e) {
+            LOG.error("Could not create / load Cambria Publisher for streamID", e);
+            return Option.none();
+        }
+    }
+
+    void closePublisherFor(String streamId) {
+        publishersCache.invalidate(streamId);
+    }
+
+
+    static class CambriaPublishersCacheLoader extends CacheLoader<String, CambriaBatchingPublisher> {
+
+        private final Map<String, PublisherConfig> dMaaPPublisherConfigs;
+
+        CambriaPublishersCacheLoader(Map<String, PublisherConfig> dMaaPPublisherConfigs) {
+            this.dMaaPPublisherConfigs = dMaaPPublisherConfigs;
+        }
+
+        @Override
+        public CambriaBatchingPublisher load(@Nonnull String streamId) {
+            return dMaaPPublisherConfigs.get(streamId)
+                .map(config -> {
+                    if (config.isSecured()) {
+                        return buildAuthenticatedPublisher(config);
+                    } else {
+                        return buildPublisher(config);
+                    }
+                }).getOrElseThrow(() -> new RuntimeException("No publisher found for streamId: " + streamId));
+        }
+
+        private CambriaBatchingPublisher buildAuthenticatedPublisher(PublisherConfig config) {
+            return unchecked(() -> builder(config)
+                .usingHttps()
+                .authenticatedByHttp(config.userName().get(), config.password().get())
+                .build()).apply();
+        }
+
+        private CambriaBatchingPublisher buildPublisher(PublisherConfig config) {
+            return unchecked(() -> builder(config).build()).apply();
+        }
+
+        private PublisherBuilder builder(PublisherConfig config) {
+            return new CambriaClientBuilders.PublisherBuilder()
+                .usingHosts(config.destinations().mkString(","))
+                .onTopic(config.topic())
+                .logSendFailuresAfter(5);
+        }
+
+    }
+
+    private static class OnPublisherRemovalListener implements RemovalListener<String, CambriaBatchingPublisher> {
+
+        private static final Logger LOG = LoggerFactory.getLogger(CambriaPublishersCache.class);
+
+        @Override
+        public void onRemoval(@Nonnull RemovalNotification<String, CambriaBatchingPublisher> notification) {
+            CambriaBatchingPublisher publisher = notification.getValue();
+            if (publisher != null) { // The value might get Garbage Collected at this moment, regardless of @Nonnull
+                try {
+                    List<?> stuck = publisher.close(20, TimeUnit.SECONDS);
+                    if (!stuck.isEmpty()) {
+                        LOG.error(format("Publisher got stuck, %s messages were dropped", stuck.size()));
+                    }
+                } catch (InterruptedException | IOException e) {
+                    LOG.error("Could not close Cambria publisher, some messages might have been dropped", e);
+                }
+            }
+        }
+    }
+
+
+}
---
+++ src/main/java/org/onap/dcae/commonFunction/event/publishing/DMaaPConfigurationParser.java
@@ -0,0 +1,110 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * org.onap.dcaegen2.collectors.ves
+ * ================================================================================
+ * Copyright (C) 2018 Nokia. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.dcae.commonFunction.event.publishing;
+
+import static io.vavr.API.$;
+import static io.vavr.API.Case;
+import static io.vavr.API.List;
+import static io.vavr.API.Try;
+import static io.vavr.API.Tuple;
+import static io.vavr.API.unchecked;
+
+import io.vavr.CheckedFunction1;
+import io.vavr.Function1;
+import io.vavr.collection.List;
+import io.vavr.collection.Map;
+import io.vavr.control.Option;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.onap.dcae.commonFunction.AnyNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Pawel Szalapski (pawel.szalapski@nokia.com)
+ */
+final class DMaaPConfigurationParser {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DMaaPConfigurationParser.class);
+    private static final Function1<String, URL> toURL = unchecked((CheckedFunction1<String, URL>) URL::new);
+
+    @SuppressWarnings("unchecked because of Javas type system limitation, actually safe to do")
+    static Map<String, PublisherConfig> parseDMaaPConfig(Path dMaaPConfigLocation) {
+        LOG.info(String.format("Building DMaaP config using configuration file: '%s'", dMaaPConfigLocation));
+        return Try(() -> AnyNode.fromString(new String(Files.readAllBytes(dMaaPConfigLocation))))
+            .peek(jsonNode -> LOG.info(f("Loading DMaaP publishers configurations based on config: '%s'", jsonNode)))
+            .mapFailure(Case($(), e -> new RuntimeException(f("Could not load DMaaPConfiguration from file: '%s'", dMaaPConfigLocation), e)))
+            .flatMap(jsonNode -> Try(() -> usesLegacyFormat(jsonNode) ? parseLegacyFormat(jsonNode) : parseNewFormat(jsonNode)))
+            .mapFailure(Case($(), e -> new RuntimeException("Parsing DMaaP configuration failed, probably unexpected format occurred", e)))
+            .peek(e -> LOG.info(f("Successfully loaded following DMaaP publishers configurations: '%s'", e)))
+            .get();
+    }
+
+    private static boolean usesLegacyFormat(AnyNode dMaaPConfig) {
+        return dMaaPConfig.has("channels");
+    }
+
+    private static Map<String, PublisherConfig> parseLegacyFormat(AnyNode root) {
+        return root.get("channels").toList().toMap(
+            channel -> channel.get("name").toString(),
+            channel -> {
+                String destinationsStr = channel.getAsOption("cambria.url")
+                    .getOrElse(channel.getAsOption("cambria.hosts").get())
+                    .toString();
+                String topic = channel.get("cambria.topic").toString();
+                Option<String> maybeUser = channel.getAsOption("basicAuthUsername").map(AnyNode::toString);
+                Option<String> maybePassword = channel.getAsOption("basicAuthPassword").map(AnyNode::toString);
+                List<String> destinations = List(destinationsStr.split(","));
+                return buildBasedOnAuth(maybeUser, maybePassword, topic, destinations);
+            });
+    }
+
+    private static Map<String, PublisherConfig> parseNewFormat(AnyNode root) {
+        return root.keys().toMap(
+            channelName -> channelName,
+            channelName -> {
+                AnyNode channelConfig = root.get(channelName);
+                Option<String> maybeUser = channelConfig.getAsOption("aaf_username").map(AnyNode::toString);
+                Option<String> maybePassword = channelConfig.getAsOption("aaf_password").map(AnyNode::toString);
+                URL topicURL = toURL.apply(channelConfig.get("dmaap_info").get("topic_url").toString());
+                String[] pathSegments = topicURL.getPath().substring(1).split("/");
+                String topic = pathSegments[1];
+                String destination = "events".equals(pathSegments[0]) ? topicURL.getAuthority() : topicURL.getHost();
+                List<String> destinations = List(destination);
+                return buildBasedOnAuth(maybeUser, maybePassword, topic, destinations);
+            });
+    }
+
+    private static PublisherConfig buildBasedOnAuth(Option<String> maybeUser,
+                                                    Option<String> maybePassword,
+                                                    String topic, List<String> destinations) {
+        return maybeUser.flatMap(user -> maybePassword.map(password -> Tuple(user, password)))
+            .map(credentials -> new PublisherConfig(destinations, topic, credentials._1, credentials._2))
+            .getOrElse(new PublisherConfig(destinations, topic));
+    }
+
+    /**
+     * Shortcut for 'string interpolation'
+     */
+    private static String f(String msg, Object... args) {
+        return String.format(msg, args);
+    }
+}
