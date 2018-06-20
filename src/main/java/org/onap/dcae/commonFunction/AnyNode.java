c5e5bc92bd7065ba1d550ceb0894e5d0f1171299
PawelSzalapski <pawel.szalapski@nokia.com>
2018-06-20 15:29:46 +0200

    Prepare codebase for dynamic DMaaP configuration
    From now on, there is only one single place
    where we can create whole app core concerning sending events
    and it has a single entry point, based on DMaaP file path.
    It can be used to rebuild part of app that is responsible
    for sending events dynamically.
    Changes are in scope for the dynamic DMaaP config feature.
    + bumped up code coverage a bit
    
    Signed-off-by: PawelSzalapski <pawel.szalapski@nokia.com>
    Issue-ID: DCAEGEN2-517
    Change-Id: I9978a76d3fc74b745096d0eb47c34055dfb0e861

--- src/main/java/org/onap/dcae/commonFunction/AnyNode.java
+++ src/main/java/org/onap/dcae/commonFunction/AnyNode.java
@@ -19,170 +19,95 @@
  */
 package org.onap.dcae.commonFunction;
 
+import static io.vavr.API.Set;
+
+import io.vavr.collection.List;
+import io.vavr.collection.Set;
+import io.vavr.control.Option;
+import java.util.stream.StreamSupport;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.JSONTokener;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 /**
- * This class is a wrapper for 2 most used entities of org.json lib: JSONArray and JSONObject and
- * comprises utility methods for fast access of json structures without need to explicitly coerce between them.
- * While using this, bear in mind it does not contain exception handling - it is assumed that when using, the parsed json structure is known.
+ * This class is a wrapper for 2 most used entities of org.json lib: JSONArray and JSONObject and comprises utility
+ * methods for fast access of json structures without need to explicitly coerce between them. While using this, bear in
+ * mind it does not contain exception handling - it is assumed that when using, the parsed json structure is known.
  *
  * @author koblosz
  */
 public class AnyNode {
-    private static final Logger LOGGER = LoggerFactory.getLogger(AnyNode.class);
+
     private Object obj;
 
-    /**
-     * @param filePath
-     * @return
-     * @throws IOException
-     */
-    public static AnyNode parse(String filePath) throws IOException {
-        try (FileReader fr = new FileReader(filePath)) {
-            JSONTokener tokener = new JSONTokener(fr);
-            return new AnyNode(new JSONObject(tokener));
-        } catch (FileNotFoundException | JSONException e1) {
-            LOGGER.error("Could not find or parse file under path %s due to: %s", filePath, e1.toString());
-            e1.printStackTrace();
-            throw e1;
-        }
+    private AnyNode(Object object) {
+        this.obj = object;
+    }
+
+    public static AnyNode fromString(String content) {
+        return new AnyNode(new JSONObject(content));
     }
 
     /**
-     * Returns keyset of underlying object. It is assumed that underlying object is of type org.json.JSONObject.
-     *
-     * @return Set of string keys present in underlying JSONObject
+     * Returns key set of underlying object. It is assumed that underlying object is of type org.json.JSONObject.
      */
-    public Set<String> getKeys() {
-        return asJsonObject().keySet();
+    public Set<String> keys() {
+        return Set(asJsonObject().keySet().toArray(new String[]{}));
     }
 
     /**
-     * Returns value associated with specified key wrapped with AnyValue object. It is assumed that this is of type org.json.JSONObject.
-     *
-     * @param key A key string
-     * @return The AnyNode object associated with given key.
+     * Returns value associated with specified key wrapped with AnyValue object. It is assumed that this is of type
+     * org.json.JSONObject.
      */
     public AnyNode get(String key) {
         return new AnyNode(asJsonObject().get(key));
     }
 
     /**
-     * Returns value under specified index wrapped with AnyValue object. It is assumed that this is of type org.json.JSONArray.
-     *
-     * @param idx An index of JSONArray
-     * @return The AnyNode object associated with given index.
+     * Returns string representation of this. If it happens to have null, the value is treated as
+     * org.json.JSONObject.NULL and "null" string is returned then.
      */
-    public AnyNode get(int idx) {
-        return new AnyNode(asJsonArray().get(idx));
-    }
-
-    /**
-     * Returns int assuming this can be coerced to int.
-     */
-    public int asInt() {
-        return (int) this.obj;
-    }
-
-    /**
-     * Returns string representation of this. If it happens to have null, the value is treated as org.json.JSONObject.NULL and "null" string is returned then.
-     *
-     * @return A String
-     */
-    public String asString() {
-        return this.obj != JSONObject.NULL ? (String) this.obj : JSONObject.NULL.toString();
-    }
-
     public String toString() {
         return this.obj.toString();
     }
 
     /**
-     * Converts underlying object to String-to-Object map. It is assumed that underlying object is of type org.json.JSONObject.
-     *
-     * @return A map.
+     * Returns optional of object under specified key, wrapped with AnyNode object.
+     * If underlying object is not of type org.json.JSONObject
+     * or underlying object has no given key
+     * or given key is null
+     * then Optional.empty will be returned.
      */
-    public Map<String, Object> asRawMap() {
-        return asJsonObject().toMap();
-    }
-
-    /**
-     * Returns optional of object under specified key, wrapped with AnyNode object. If underlying object is not of type org.json.JSONObject, then Optional.empty will be returned.
-     *
-     * @param key A key string
-     */
-    public Optional<AnyNode> getAsOptional(String key) {
-        AnyNode result = null;
+    public Option<AnyNode> getAsOption(String key) {
         try {
-            result = get(key);
+            AnyNode value = get(key);
+            if (value.toString().equals("null")) {
+                return Option.none();
+            }
+            return Option.some(value);
         } catch (JSONException ex) {
+            return Option.none();
         }
-        return Optional.ofNullable(result);
-    }
-
-    public JSONObject asJsonObject() {
-        return (JSONObject) this.obj;
     }
 
     /**
-     * Converts underlying object to map representation with map values wrapped with AnyNode object. It is assumed that underlying object is of type org.json.JSONObject.
+     * Converts underlying object to map representation with map values wrapped with AnyNode object. It is assumed that
+     * underlying object is of type org.json.JSONObject.
      */
-    public Map<String, AnyNode> asMap() {
-        Map<String, AnyNode> map = new HashMap<>();
-        getKeys().forEach(key -> map.put(key, get(key)));
-        return map;
-    }
-
-    /**
-     * Converts underlying object to map representation with map values wrapped with AnyNode object. It is assumed that underlying object is of type org.json.JSONObject.
-     */
-    public java.util.List<AnyNode> asList() {
-        return asStream().collect(Collectors.toList());
-    }
-
-    /**
-     * Converts this object to stream of underlying objects wrapped with AnyNode class. It is assumed that this is of type JSONArray.
-     */
-    public Stream<AnyNode> asStream() {
-        return StreamSupport.stream(((JSONArray) this.obj).spliterator(), false).map(AnyNode::new);
+    public List<AnyNode> toList() {
+        return List.ofAll(StreamSupport.stream(((JSONArray) this.obj).spliterator(), false).map(AnyNode::new));
     }
 
     /**
      * Checks if specified key is present in this. It is assumed that this is of type JSONObject.
      */
-    public boolean hasKey(String key) {
-        return getAsOptional(key).isPresent();
+    public boolean has(String key) {
+        return !getAsOption(key).isEmpty();
     }
 
-    /**
-     * Returns empty AnyNode (with null inside)
-     */
-    public static AnyNode nullValue() {
-        return new AnyNode(JSONObject.NULL.toString());
+    private JSONObject asJsonObject() {
+        return (JSONObject) this.obj;
     }
 
-    private JSONArray asJsonArray() {
-        return (JSONArray) this.obj;
-    }
-
-    private AnyNode(Object object) {
-        this.obj = object;
-    }
 
 }
