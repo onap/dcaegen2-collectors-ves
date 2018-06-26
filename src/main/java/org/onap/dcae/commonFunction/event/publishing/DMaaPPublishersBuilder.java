fa32ad4cc753e992d7b2c1e6896cebb3083393fa
PawelSzalapski <pawel.szalapski@nokia.com>
2018-06-26 15:16:41 +0200

    Prepare codebase for dynamic DMaaP configuration
    From now on, there is only one single place
    where we can create whole app core concerning sending events
    and it has a single entry point, based on DMaaP configuration.
    It can be used to rebuild part of app that is responsible
    for sending events dynamically.
    Changes are in scope for the dynamic DMaaP config feature.
    + bumped up code coverage a bit
    
    Change-Id: Iecc8c4e534ae9b781f47e3616409271ba83169c8
    Signed-off-by: PawelSzalapski <pawel.szalapski@nokia.com>
    Issue-ID: DCAEGEN2-517

---
+++ src/main/java/org/onap/dcae/commonFunction/event/publishing/DMaaPPublishersBuilder.java
@@ -0,0 +1,62 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * org.onap.dcaegen2.collectors.ves
+ * ================================================================================
+ * Copyright (C) 2018 Nokia. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.dcae.commonFunction.event.publishing;
+
+import static io.vavr.API.Try;
+import static org.onap.dcae.commonFunction.event.publishing.VavrUtils.enhanceError;
+import static org.onap.dcae.commonFunction.event.publishing.VavrUtils.f;
+
+import com.att.nsa.cambria.client.CambriaBatchingPublisher;
+import com.att.nsa.cambria.client.CambriaClientBuilders;
+import com.att.nsa.cambria.client.CambriaClientBuilders.PublisherBuilder;
+import io.vavr.control.Try;
+
+/**
+ * @author Pawel Szalapski (pawel.szalapski@nokia.com)
+ */
+final class DMaaPPublishersBuilder {
+
+    @SuppressWarnings("mapFailure takes a generic varargs, unchecked because of Javas type system limitation, actually safe to do")
+    static Try<CambriaBatchingPublisher> buildPublisher(PublisherConfig config) {
+        return Try(() -> builder(config).build())
+            .mapFailure(enhanceError(f("DMaaP client builder throws exception for this configuration: '%s'", config)));
+    }
+
+    private static PublisherBuilder builder(PublisherConfig config) {
+        if (config.isSecured()) {
+            return authenticatedBuilder(config);
+        } else {
+            return unAuthenticatedBuilder(config);
+        }
+    }
+
+    private static PublisherBuilder authenticatedBuilder(PublisherConfig config) {
+        return unAuthenticatedBuilder(config)
+            .usingHttps()
+            .authenticatedByHttp(config.userName().get(), config.password().get());
+    }
+
+    private static PublisherBuilder unAuthenticatedBuilder(PublisherConfig config) {
+        return new CambriaClientBuilders.PublisherBuilder()
+            .usingHosts(config.destinations().mkString(","))
+            .onTopic(config.topic())
+            .logSendFailuresAfter(5);
+    }
+}
