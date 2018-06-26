1234e8527b5b6fdc1e5e01de7670e09af06f5593
PawelSzalapski <pawel.szalapski@nokia.com>
2018-06-26 15:16:41 +0200

    Prepare codebase for dynamic DMaaP configuration
    From now on, there is only one single place
    where we can create whole app core concerning sending events
    and it has a single entry point, based on DMaaP configuration.
    It can be used to rebuild part of app that is responsible
    for sending events dynamically.
    Changes are in scope for the dynamic DMaaP config feature.
    + bumped up code coverage a bit
    
    Change-Id: Iecc8c4e534ae9b781f47e3616409271ba83169c8
    Signed-off-by: PawelSzalapski <pawel.szalapski@nokia.com>
    Issue-ID: DCAEGEN2-517

--- src/test/java/org/onap/dcae/commonFunction/event/publishing/DMaaPEventPublisherTest.java
+++ src/test/java/org/onap/dcae/commonFunction/event/publishing/DMaaPEventPublisherTest.java
@@ -17,74 +17,74 @@
  * limitations under the License.
  * ============LICENSE_END=========================================================
  */
-package org.onap.dcae.commonFunction;
+package org.onap.dcae.commonFunction.event.publishing;
 
+import static io.vavr.API.Option;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import com.att.nsa.cambria.client.CambriaBatchingPublisher;
 import java.io.IOException;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.slf4j.Logger;
 
-@RunWith(MockitoJUnitRunner.class)
-public class EventPublisherHashTest {
-    private EventPublisherHash cut;
+public class DMaaPEventPublisherTest {
 
-    @Mock
-    private DmaapPublishers dmaapPublishers;
-    @Mock
+    private DMaaPEventPublisher eventPublisher;
     private CambriaBatchingPublisher cambriaPublisher;
+    private DMaaPPublishersCache DMaaPPublishersCache;
 
     @Before
     public void setUp() {
-        given(dmaapPublishers.getByStreamId(anyString())).willReturn(cambriaPublisher);
-
-        cut = new EventPublisherHash(dmaapPublishers);
+        cambriaPublisher = mock(CambriaBatchingPublisher.class);
+        DMaaPPublishersCache = mock(DMaaPPublishersCache.class);
+        when(DMaaPPublishersCache.getPublisher(anyString())).thenReturn(Option(cambriaPublisher));
+        eventPublisher = new DMaaPEventPublisher(DMaaPPublishersCache, mock(Logger.class));
     }
 
     @Test
-    public void sendEventShouldSendEventToATopic() throws Exception {
+    public void shouldSendEventToTopic() throws Exception {
         // given
         JSONObject event = new JSONObject("{}");
         final String streamId = "sampleStreamId";
 
         // when
-        cut.sendEvent(event, streamId);
+        eventPublisher.sendEvent(event, streamId);
 
         // then
         verify(cambriaPublisher).send("MyPartitionKey", event.toString());
     }
 
     @Test
-    public void sendEventShouldRemoveUuid() throws Exception {
+    public void shouldRemoveInternalVESUIDBeforeSending() throws Exception {
         // given
-        JSONObject event = new JSONObject("{\"VESuniqueId\": \"362e0146-ec5f-45f3-8d8f-bfe877c3f58e\", \"another\": 8}");
+        JSONObject event = new JSONObject(
+            "{\"VESuniqueId\": \"362e0146-ec5f-45f3-8d8f-bfe877c3f58e\", \"another\": 8}");
         final String streamId = "sampleStreamId";
 
         // when
-        cut.sendEvent(event, streamId);
+        eventPublisher.sendEvent(event, streamId);
 
         // then
         verify(cambriaPublisher).send("MyPartitionKey", new JSONObject("{\"another\": 8}").toString());
     }
 
     @Test
-    public void sendEventShouldCloseConnectionWhenExceptionOccurred() throws Exception {
+    public void shouldCloseConnectionWhenExceptionOccurred() throws Exception {
         // given
         JSONObject event = new JSONObject("{}");
         final String streamId = "sampleStreamId";
         given(cambriaPublisher.send(anyString(), anyString())).willThrow(new IOException("epic fail"));
 
         // when
-        cut.sendEvent(event, streamId);
+        eventPublisher.sendEvent(event, streamId);
 
         // then
-        verify(dmaapPublishers).closeByStreamId(streamId);
+        verify(DMaaPPublishersCache).closePublisherFor(streamId);
     }
 }
