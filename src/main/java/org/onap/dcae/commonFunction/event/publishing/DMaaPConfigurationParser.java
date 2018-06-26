7b063c9ab543515ecd3fd768ea282a8a7e74e8d1
PawelSzalapski <pawel.szalapski@nokia.com>
2018-06-26 15:16:41 +0200

    Prepare codebase for dynamic DMaaP configuration
    From now on, there is only one single place
    where we can create whole app core concerning sending events
    and it has a single entry point, based on DMaaP configuration.
    It can be used to rebuild part of app that is responsible
    for sending events dynamically.
    Changes are in scope for the dynamic DMaaP config feature.
    + bumped up code coverage a bit
    
    Change-Id: Iecc8c4e534ae9b781f47e3616409271ba83169c8
    Signed-off-by: PawelSzalapski <pawel.szalapski@nokia.com>
    Issue-ID: DCAEGEN2-517

---
+++ src/main/java/org/onap/dcae/commonFunction/event/publishing/DMaaPConfigurationParser.java
@@ -0,0 +1,107 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * org.onap.dcaegen2.collectors.ves
+ * ================================================================================
+ * Copyright (C) 2018 Nokia. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.dcae.commonFunction.event.publishing;
+
+import static io.vavr.API.List;
+import static io.vavr.API.Try;
+import static io.vavr.API.Tuple;
+import static io.vavr.API.unchecked;
+import static org.onap.dcae.commonFunction.event.publishing.VavrUtils.enhanceError;
+import static org.onap.dcae.commonFunction.event.publishing.VavrUtils.f;
+
+import io.vavr.collection.List;
+import io.vavr.collection.Map;
+import io.vavr.control.Option;
+import io.vavr.control.Try;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.onap.dcae.commonFunction.AnyNode;
+
+/**
+ * @author Pawel Szalapski (pawel.szalapski@nokia.com)
+ */
+@SuppressWarnings("mapFailure takes a generic varargs, unchecked because of Javas type system limitation, actually safe to do")
+public final class DMaaPConfigurationParser {
+
+    public static Try<Map<String, PublisherConfig>> parseToDomainMapping(Path configLocation) {
+        return readFromFile(configLocation)
+            .flatMap(DMaaPConfigurationParser::toJSON)
+            .flatMap(DMaaPConfigurationParser::toConfigMap);
+    }
+
+    private static Try<String> readFromFile(Path configLocation) {
+        return Try(() -> new String(Files.readAllBytes(configLocation)))
+            .mapFailure(enhanceError(f("Could not read DMaaP configuration from location: '%s'", configLocation)));
+    }
+
+    private static Try<AnyNode> toJSON(String config) {
+        return Try(() -> AnyNode.fromString(config))
+            .mapFailure(enhanceError(f("DMaaP configuration '%s' is not a valid JSON document", config)));
+    }
+
+    private static Try<Map<String, PublisherConfig>> toConfigMap(AnyNode config) {
+        return Try(() -> usesLegacyFormat(config) ? parseLegacyFormat(config) : parseNewFormat(config))
+            .mapFailure(enhanceError(f("Parsing DMaaP configuration: '%s' failed, probably it is in unexpected format", config)));
+    }
+
+    private static boolean usesLegacyFormat(AnyNode dMaaPConfig) {
+        return dMaaPConfig.has("channels");
+    }
+
+    private static Map<String, PublisherConfig> parseLegacyFormat(AnyNode root) {
+        return root.get("channels").toList().toMap(
+            channel -> channel.get("name").toString(),
+            channel -> {
+                String destinationsStr = channel.getAsOption("cambria.url")
+                    .getOrElse(channel.getAsOption("cambria.hosts").get())
+                    .toString();
+                String topic = channel.get("cambria.topic").toString();
+                Option<String> maybeUser = channel.getAsOption("basicAuthUsername").map(AnyNode::toString);
+                Option<String> maybePassword = channel.getAsOption("basicAuthPassword").map(AnyNode::toString);
+                List<String> destinations = List(destinationsStr.split(","));
+                return buildBasedOnAuth(maybeUser, maybePassword, topic, destinations);
+            });
+    }
+
+    private static Map<String, PublisherConfig> parseNewFormat(AnyNode root) {
+        return root.keys().toMap(
+            channelName -> channelName,
+            channelName -> {
+                AnyNode channelConfig = root.get(channelName);
+                Option<String> maybeUser = channelConfig.getAsOption("aaf_username").map(AnyNode::toString);
+                Option<String> maybePassword = channelConfig.getAsOption("aaf_password").map(AnyNode::toString);
+                URL topicURL = unchecked(() -> new URL(channelConfig.get("dmaap_info").get("topic_url").toString())).apply();
+                String[] pathSegments = topicURL.getPath().substring(1).split("/");
+                String topic = pathSegments[1];
+                String destination = "events".equals(pathSegments[0]) ? topicURL.getAuthority() : topicURL.getHost();
+                List<String> destinations = List(destination);
+                return buildBasedOnAuth(maybeUser, maybePassword, topic, destinations);
+            });
+    }
+
+    private static PublisherConfig buildBasedOnAuth(Option<String> maybeUser,
+                                                    Option<String> maybePassword,
+                                                    String topic, List<String> destinations) {
+        return maybeUser.flatMap(user -> maybePassword.map(password -> Tuple(user, password)))
+            .map(credentials -> new PublisherConfig(destinations, topic, credentials._1, credentials._2))
+            .getOrElse(new PublisherConfig(destinations, topic));
+    }
+}
---
+++ src/test/java/org/onap/dcae/commonFunction/event/publishing/DMaaPConfigurationParserTest.java
@@ -0,0 +1,113 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * org.onap.dcaegen2.collectors.ves
+ * ================================================================================
+ * Copyright (C) 2017-2018 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2018 Nokia. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.dcae.commonFunction.event.publishing;
+
+import static io.vavr.API.List;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.onap.dcae.commonFunction.event.publishing.DMaaPConfigurationParser.parseToDomainMapping;
+
+import io.vavr.collection.Map;
+import io.vavr.control.Try;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.junit.Test;
+
+/**
+ * @author Pawel Szalapski (pawel.szalapski@nokia.com)
+ */
+public class DMaaPConfigurationParserTest {
+
+    @Test
+    public void testParseCredentialsForGen2() {
+        Path path = Paths.get("src/test/resources/testParseDMaaPCredentialsGen2.json");
+        Try<Map<String, PublisherConfig>> publisherConfigs = parseToDomainMapping(path);
+
+        PublisherConfig authCredentialsNulls = publisherConfigs.get().get("auth-credentials-null").getOrNull();
+        assertThat(authCredentialsNulls.userName().isEmpty()).isTrue();
+        assertThat(authCredentialsNulls.password().isEmpty()).isTrue();
+        assertThat(authCredentialsNulls.isSecured()).isFalse();
+
+        PublisherConfig authCredentialsPresent = publisherConfigs.get().get("auth-credentials-present").getOrNull();
+        assertThat(authCredentialsPresent.userName().getOrNull()).isEqualTo("sampleUser");
+        assertThat(authCredentialsPresent.password().getOrNull()).isEqualTo("samplePassword");
+        assertThat(authCredentialsPresent.isSecured()).isTrue();
+
+        PublisherConfig authCredentialsKeysMissing = publisherConfigs.get().get("auth-credentials-missing").getOrNull();
+        assertThat(authCredentialsKeysMissing.userName().isEmpty()).isTrue();
+        assertThat(authCredentialsKeysMissing.password().isEmpty()).isTrue();
+        assertThat(authCredentialsKeysMissing.isSecured()).isFalse();
+    }
+
+
+    @Test
+    public void testParseCredentialsForLegacy() {
+        Path path = Paths.get("src/test/resources/testParseDMaaPCredentialsLegacy.json");
+        Try<Map<String, PublisherConfig>> publisherConfigs = parseToDomainMapping(path);
+
+        PublisherConfig authCredentialsNull = publisherConfigs.get().get("auth-credentials-null").getOrNull();
+        assertThat(authCredentialsNull.userName().isEmpty()).isTrue();
+        assertThat(authCredentialsNull.password().isEmpty()).isTrue();
+        assertThat(authCredentialsNull.isSecured()).isFalse();
+
+        PublisherConfig authCredentialsPresent = publisherConfigs.get().get("auth-credentials-present").getOrNull();
+        assertThat(authCredentialsPresent.userName().getOrNull()).isEqualTo("sampleUser");
+        assertThat(authCredentialsPresent.password().getOrNull()).isEqualTo("samplePassword");
+        assertThat(authCredentialsPresent.isSecured()).isTrue();
+
+        PublisherConfig authCredentialsMissing = publisherConfigs.get().get("auth-credentials-missing").getOrNull();
+        assertThat(authCredentialsMissing.userName().isEmpty()).isTrue();
+        assertThat(authCredentialsMissing.password().isEmpty()).isTrue();
+        assertThat(authCredentialsMissing.isSecured()).isFalse();
+    }
+
+
+    @Test
+    public void testParseGen2() {
+        Path path = Paths.get("src/test/resources/testParseDMaaPGen2.json");
+        Try<Map<String, PublisherConfig>> publisherConfigs = parseToDomainMapping(path);
+
+        PublisherConfig withEventsSegment = publisherConfigs.get().get("event-segments-with-port").getOrNull();
+        assertThat(withEventsSegment.destinations()).isEqualTo(List("UEBHOST:3904"));
+        assertThat(withEventsSegment.topic()).isEqualTo("DCAE-SE-COLLECTOR-EVENTS-DEV");
+
+        PublisherConfig withOtherSegment = publisherConfigs.get().get("other-segments-without-ports").getOrNull();
+        assertThat(withOtherSegment.destinations()).isEqualTo(List("UEBHOST"));
+        assertThat(withOtherSegment.topic()).isEqualTo("DCAE-SE-COLLECTOR-EVENTS-DEV");
+    }
+
+    @Test
+    public void testParseLegacy() {
+        Path exemplaryConfig = Paths.get("src/test/resources/testParseDMaaPLegacy.json");
+        Try<Map<String, PublisherConfig>> publisherConfigs = DMaaPConfigurationParser.parseToDomainMapping(exemplaryConfig);
+
+        PublisherConfig urlFirstThenHosts = publisherConfigs.get().get("url-precedes-hosts").getOrNull();
+        assertThat(urlFirstThenHosts.destinations()).isEqualTo(List("127.0.0.1:3904"));
+        assertThat(urlFirstThenHosts.topic()).isEqualTo("DCAE-SE-COLLECTOR-EVENTS-DEV");
+
+        PublisherConfig urlKeyMissing = publisherConfigs.get().get("url-key-missing").getOrNull();
+        assertThat(urlKeyMissing.destinations()).isEqualTo(List("h1.att.com", "h2.att.com"));
+        assertThat(urlKeyMissing.topic()).isEqualTo("DCAE-SE-COLLECTOR-EVENTS-DEV");
+
+        PublisherConfig urlIsMissing = publisherConfigs.get().get("url-is-null").getOrNull();
+        assertThat(urlIsMissing.destinations()).isEqualTo(List("h1.att.com", "h2.att.com"));
+        assertThat(urlIsMissing.topic()).isEqualTo("DCAE-SE-COLLECTOR-EVENTS-DEV");
+    }
+}
