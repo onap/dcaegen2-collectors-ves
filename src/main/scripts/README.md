9d32cf8996d64c6ae0c34e740fbd29e2b43f69d5
PawelSzalapski <pawel.szalapski@nokia.com>
2018-07-10 13:25:18 +0200

    Clean up .sh scripts
    Removed .sh scripts that are not needed.
    Created a devScripts package to store things used locally.
    Add readme with a description of some of the things
    that might be not visible at first glance
    Refactor .sh scripts, remove not used or not needed parts
    Fix a bug where pidof <package> does not work.
    Change logging of the scripts and they way that app is run so
    that the logs are in one place -> collector.logs
    
    Change-Id: I156328ab23d18a20072177ae8c5420772c3e2c7c
    Signed-off-by: PawelSzalapski <pawel.szalapski@nokia.com>
    Issue-ID: DCAEGEN2-610

---
+++ src/main/scripts/README.md
@@ -0,0 +1,53 @@
+## What this is directory for
+This directory contains .sh scripts that are used to start & stop & configure the VESCollector application
+inside the docker image.
+These scripts are packaged inside the docker by a mvn assembly & docker plugins.
+
+## How the application starts:
+General flow goes like this
+- Docker image is build, and it points docker-entry.sh as the entrypoing.
+- Docker-entry point, depending on the deployment type, 
+configures a bunch of things and starts the application in a separate process
+and loops indefinitely to hold the docker container process.
+
+## Environment variables
+Most of the configuration of how VESCollector should be started and managed is done through environment variables.
+Some of them are set during the image build process and some of them are defined manually or by
+a particular deployment system.
+
+These include: <br>
+
+Image build time variables
+- HOSTALIASES
+- SCRIPTS_PATH - points to a directory INSIDE the docker container that contains all the scripts, as they are put inside one single directory
+- LOGS_PATH - points to a directory where all logs should be stored
+- JAVA_EXECUTABLE - points to a java executable inside the container
+- MAIN_LOG_FILE - points to a main log file. Output from all .sh scripts are going to the standard console and to that file.
+- APP_BASE_PATH - points to file system path that is considered to be a root of the VESCollector application
+- CONFIG_FILES_PATH - points to file system path where configuration files lie
+- LIBRARIES_PATH - points to file system path where necessary .jar files lie
+ 
+Variables set manually / coming from deployment system:
+- COLLECTOR_IP
+- DMAAPHOST - set outside the code of VESCollector, should contain an address to DMaaP, so that event publishing can work
+- CBSPOLLTIMER - it should be put in here, if we want to automatically fetch configuration from CBS.
+- CONSUL_HOST - used with conjunction with CBSPOLLTIMER, should be a host address (without port! e.g http://my-ip-or-host) where Consul service lies
+- CONFIG_BINDING_SERVICE - should be a name of CBS as it is registered in Consul
+- HOSTNAME - should be a name of VESCollector application as it is registered in CBS catalog
+
+## Development scripts
+Inside the scripts/devScripts we have a place for all utility scripts that are useful during the development
+of VESCollector. These scripts should not be put into an image but can be saved into repository for a developers convenience.
+
+## Docker file system layout
+The main directory where all code resides in docker container, denoted by $APP_BASE_PATH
+looks like this and is located in /opt/app/VESCollector
+```
+<host>:/opt/app/VESCollector# ls
+Dockerfile  bin  etc  lib  logs  specs	tomcat.8080
+```
+- bin contains sh scripts (path here is denoted by env var $SCRIPTS_PATH)
+- etc contain various application configuration, most notably it reflects 'etc' directory from repository
+- lib contains all libraries that are pulled into the app during maven build
+- logs contain all application logs, especially collector.log file which is a main log file denoted by $MAIN_LOG_FILE variable
+- spec contain json schemas specs for ves-collector
--- src/main/scripts/docker-entry.sh
+++ src/main/scripts/docker-entry.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 ###
 # ============LICENSE_START=======================================================
 # PROJECT
 # ================================================================================
 # Copyright (C) 2017-2018 AT&T Intellectual Property. All rights reserved.
+# Copyright (C) 2018 Nokia Networks Intellectual Property. All rights reserved.
 # ================================================================================
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,36 +19,41 @@
 # limitations under the License.
 # ============LICENSE_END=========================================================
 ###
+source ${SCRIPTS_PATH}/logger.sh
 
-if [ ! -z "$COLLECTOR_IP" ]; then
-        echo $COLLECTOR_IP  $(hostname).dcae.simpledemo.onap.org >> /etc/hosts
+# Redirect all stdout & stderr to a main log file, but also let it print into the console
+# At the time this script is invoked, these directories and files do not exist yet, so we need to create them
+mkdir -p ${LOGS_PATH}
+touch ${MAIN_LOG_FILE}
+exec &> >(tee -a ${MAIN_LOG_FILE})
+
+log "Main application entry-point invoked"
+
+if [ ! -z ${COLLECTOR_IP} ]; then
+    log "Collector ip (${COLLECTOR_IP}) (env var 'COLLECTOR_IP') found, adding entry to /etc/hosts"
+    echo ${COLLECTOR_IP}  $(hostname).dcae.simpledemo.onap.org >> /etc/hosts
 fi
 
-if [ ! -z "$DMAAPHOST" ]; then
-        if [ -z "$(echo $DMAAPHOST | sed -e 's/[0-9\.]//g')" ]; then 
-                echo "$DMAAPHOST  onap-dmaap" >> /etc/hosts
-        else
-                echo "onap-dmaap $DMAAPHOST" >> /etc/host.aliases
-        fi
+if [ ! -z ${DMAAPHOST} ]; then
+    if [ -z "$(echo ${DMAAPHOST} | sed -e 's/[0-9\.]//g')" ]; then
+        log "DMaaP host (${DMAAPHOST}) (env var 'DMAAPHOST') found, adding entry to /etc/hosts"
+        echo "${DMAAPHOST}  onap-dmaap" >> /etc/hosts
+    else
+        log "DMaaP host (${DMAAPHOST}) (env var 'DMAAPHOST') found, adding entry to /etc/host.aliases"
+        echo "onap-dmaap ${DMAAPHOST}" >> /etc/host.aliases
+    fi
 else
-	echo "DMAAPHOST ENV NOT SET!! PUBLISH WILL NOT BE SUPPORTED"
+	logWarn "DMaaP host (env var 'DMAAPHOST') is missing. Events will not be published to DMaaP"
 fi
 
-if [ -z "$CONSUL_HOST" ] || [ -z "$CONFIG_BINDING_SERVICE" ] || [ -z "$HOSTNAME" ]; then
-                echo "INFO: USING STANDARD ALONE CONFIGURATION SETUP"
-		## For Container supporting both classic and GEN2 controller - below line should be uncommented, provided service manager package is included
-		#/opt/app/manager/start-manager.sh
-else
-                echo "INFO: USING DCAEGEN2 CONTROLLER"
-fi
+log "Scheduling application to be started, looping indefinitely to hold the docker process"
+${SCRIPTS_PATH}/VESrestfulCollector.sh stop
+${SCRIPTS_PATH}/VESrestfulCollector.sh start &
 
-/opt/app/VESCollector/bin/VESrestfulCollector.sh stop
-/opt/app/VESCollector/bin/VESrestfulCollector.sh start &
-
-# Add below if config polling should be enabled
-# More specific to K8 deployment in ONAP
-if [ ! -z "$CBSPOLLTIMER" ]; then
-	/opt/app/VESCollector/bin/VESConfigPoller.sh &
+# Add below if config polling should be enabled. More specific to K8 deployment in ONAP
+if [ ! -z ${CBSPOLLTIMER} ]; then
+    log "Configuration poll time (${CBSPOLLTIMER}) (env var 'CBSPOLLTIMER') found, enabling configuration polling from CBS"
+    ${SCRIPTS_PATH}/VESConfigPoller.sh &
 fi
 
 while true; do sleep 1000; done
