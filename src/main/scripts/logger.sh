f6362cd4790289629761df5fec2a2aa3bd3c9c10
PawelSzalapski <pawel.szalapski@nokia.com>
2018-07-10 13:25:18 +0200

    Clean up .sh scripts
    Removed .sh scripts that are not needed.
    Created a devScripts package to store things used locally.
    Add readme with a description of some of the things
    that might be not visible at first glance
    Refactor .sh scripts, remove not used or not needed parts
    Fix a bug where pidof <package> does not work.
    Change logging of the scripts and they way that app is run so
    that the logs are in one place -> collector.logs
    
    Change-Id: I156328ab23d18a20072177ae8c5420772c3e2c7c
    Signed-off-by: PawelSzalapski <pawel.szalapski@nokia.com>
    Issue-ID: DCAEGEN2-610

--- src/main/scripts/logger.sh
+++ src/main/scripts/logger.sh
@@ -1,15 +1,16 @@
+#!/bin/bash
 ###
 # ============LICENSE_START=======================================================
 # PROJECT
 # ================================================================================
-# Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+# Copyright (C) 2018 Nokia Networks Intellectual Property. All rights reserved.
 # ================================================================================
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,24 +19,12 @@
 # ============LICENSE_END=========================================================
 ###
 
-#!/bin/sh
+# Mimics the log4j formatter so that is is consistent with what is currently in that log file
 
-#secPid=`pgrep -f com.att.dcae.commonFunction.CommonStartup` --> master
-secPid=`pgrep -f org.onap.dcae.commonFunction.CommonStartup`
+log() {
+	echo "[$(date -u +'%Y-%m-%d %H:%M:%S,%3N')][INFO][PID:$$][$0] $1"
+}
 
-
-if [ "${secPid}" ]
-then
-        #errorcnt = `grep -c "CambriaSimplerBatchPublisher - Send failed" ../logs/collector.log`
-        errorcnt=`tail -1000 ../logs/collector.log | grep -c "CambriaSimplerBatchPublisher - Send failed"`
-        
-        if [ $errorcnt -gt 10 ]
-        then
-                echo "VESCollecter_Is_HavingError to publish"
-        else
-                echo "VESCollecter_Is_Running as PID $secPid"
-        fi
-else
-   echo "VESCollecter_Is_Not_Running"
-fi
-exit
+logWarn() {
+	echo "[$(date -u +'%Y-%m-%d %H:%M:%S,%3N')][WARN][PID:$$][$0] $1"
+}
