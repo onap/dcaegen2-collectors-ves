9a07a2c23e4ad44a65096004b2731b7188a8d09c
andrzejszukuc <andrzej.szukuc@nokia.com>
2018-10-04 09:48:10 +0200

    ApiAuthInterceptor tests have been added
    Change-Id: Ieb42e8fa417ff3afb8acad3bd6c48a8287026a2e
    Issue-ID: DCAEGEN2-517
    Signed-off-by: ANDRZEJ SZUKUC <andrzej.szukuc@nokia.com>

--- src/main/java/org/onap/dcae/restapi/ApiAuthInterceptor.java
+++ src/main/java/org/onap/dcae/restapi/ApiAuthInterceptor.java
@@ -23,9 +23,6 @@ import io.vavr.control.Option;
 import org.onap.dcae.ApplicationSettings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
