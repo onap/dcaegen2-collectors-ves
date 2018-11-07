ccc8092edac6296abb0ee975f4343b73411482e2
andrzejszukuc <andrzej.szukuc@nokia.com>
2018-11-07 12:51:05 +0100

    TLS mutual authentication has been added.
    Change-Id: I60ebe8e1b06d72413940935396cb7a56af437c0d
    Issue-ID: DCAEGEN2-959
    Signed-off-by: ANDRZEJ SZUKUC <andrzej.szukuc@nokia.com>

--- src/main/java/org/onap/dcae/restapi/ServletConfig.java
+++ src/main/java/org/onap/dcae/restapi/ServletConfig.java
@@ -45,15 +45,16 @@ public class ServletConfig implements WebServerFactoryCustomizer<ConfigurableSer
 
     @Override
     public void customize(ConfigurableServletWebServerFactory container) {
-        if (properties.authorizationEnabled()) {
-            container.setSsl(createSSL());
+        final boolean hasClientTslAuthorization = properties.clientTlsAuthorizationEnabled();
+        if (hasClientTslAuthorization || properties.authorizationEnabled()) {
+            container.setSsl(createSSL(hasClientTslAuthorization));
             container.setPort(properties.httpsPort());
         } else {
             container.setPort(properties.httpPort());
         }
     }
 
-    private Ssl createSSL() {
+    private Ssl createSSL(final boolean hasClientTslAuthorization) {
         log.info("Enabling SSL");
         Ssl ssl = new Ssl();
         ssl.setEnabled(true);
@@ -64,6 +65,27 @@ public class ServletConfig implements WebServerFactoryCustomizer<ConfigurableSer
         log.info("Using keyStore password from: " + keyPasswordFileLocation);
         ssl.setKeyPassword(getKeyStorePassword(keyPasswordFileLocation));
         ssl.setKeyAlias(properties.keystoreAlias());
+
+        if (hasClientTslAuthorization) {
+            log.info("Enabling TLS client authorization");
+
+            final String trustStore =
+                    Paths.get(properties.truststoreFileLocation())
+                            .toAbsolutePath()
+                            .toString();
+            log.info("Using trustStore path: " + trustStore);
+
+            ssl.setTrustStore(trustStore);
+
+            final String trustPasswordFileLocation =
+                    Paths.get(properties.truststorePasswordFileLocation())
+                            .toAbsolutePath()
+                            .toString();
+            log.info("Using trustStore password from: " + trustPasswordFileLocation);
+            ssl.setTrustStorePassword(getKeyStorePassword(trustPasswordFileLocation));
+            ssl.setClientAuth(Ssl.ClientAuth.NEED);
+        }
+
         return ssl;
     }
 
