532738ce191bb914972e37201a2223fbb25d27c9
Zlatko Murgoski <zlatko.murgoski@nokia.com>
2019-03-05 11:31:48 +0100

    Collector authentication enhancement
    Collector authentication enhancement
    
    Change-Id: I03a05cb83dd8c498fb218e82e9b3958348fbb4ac
    Issue-ID: DCAEGEN2-1101
    Signed-off-by: Zlatko Murgoski <zlatko.murgoski@nokia.com>

---
+++ src/main/java/org/onap/dcae/common/configuration/CertBasicAuth.java
@@ -0,0 +1,54 @@
+/*
+ * ============LICENSE_START=======================================================
+ * PROJECT
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2018 Nokia. All rights reserved.s
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.dcae.common.configuration;
+
+import org.onap.dcae.ApplicationException;
+import org.onap.dcae.ApplicationSettings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+
+public class CertBasicAuth implements AuthMethod {
+
+  private static final Logger log = LoggerFactory.getLogger(CertBasicAuth.class);
+
+  private ConfigurableServletWebServerFactory container;
+  private ApplicationSettings properties;
+
+  public CertBasicAuth(ConfigurableServletWebServerFactory container, ApplicationSettings properties) {
+    this.container = container;
+    this.properties = properties;
+  }
+
+  @Override
+  public void configure() {
+    SslContextCreator sslContextCreator = new SslContextCreator(properties);
+    if(properties.authorizationEnabled()){
+      container.setPort(properties.httpsPort());
+      log.info("Application work on https port: " + properties.httpsPort());
+      container.setSsl(sslContextCreator.httpsContextWithTlsAuthentication());
+    }
+    else {
+      throw new ApplicationException("Header authflag have to be enabled to work with base64 and TLS");
+    }
+  }
+}
