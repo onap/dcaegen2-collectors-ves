686738274b70e51b78d3677e21d1a2457e273bbd
Zlatko Murgoski <zlatko.murgoski@nokia.com>
2019-03-08 12:56:43 +0100

    Remove unused method
    Remove unused method
    
    Issue-ID: DCAEGEN2-1016
    Change-Id: Iac2a295c35f6a659442decc158effb7db2f93039
    Signed-off-by: Zlatko Murgoski <zlatko.murgoski@nokia.com>

--- src/main/java/org/onap/dcae/common/VESLogger.java
+++ src/main/java/org/onap/dcae/common/VESLogger.java
@@ -25,31 +25,13 @@ import com.att.nsa.clock.SaClock;
 import com.att.nsa.logging.LoggingContext;
 import com.att.nsa.logging.LoggingContextFactory;
 import com.att.nsa.logging.log4j.EcompFields;
-import jline.internal.Log;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.UUID;
 
 public class VESLogger {
 
-    public static final String VES_AGENT = "VES_AGENT";
     public static final String REQUEST_ID = "requestId";
-    private static final String IP_ADDRESS = "127.0.0.1";
-    private static final String HOST_NAME = "localhost";
-
-    public static Logger auditLog;
-    public static Logger metricsLog;
-    public static Logger errorLog;
-    public static Logger debugLog;
-
-    // Common LoggingContext
     private static LoggingContext commonLC;
-    // Thread-specific LoggingContext
     private static LoggingContext threadLC;
-    public LoggingContext lc;
 
     /**
      * Returns the common LoggingContext instance that is the base context for
@@ -116,45 +98,4 @@ public class VESLogger {
         threadLC.put(EcompFields.kEndTimestamp, SaClock.now());
         return threadLC;
     }
-
-    public static void setUpEcompLogging() {
-
-        // Create ECOMP Logger instances
-        auditLog = LoggerFactory.getLogger("com.att.ecomp.audit");
-        metricsLog = LoggerFactory.getLogger("com.att.ecomp.metrics");
-        debugLog = LoggerFactory.getLogger("com.att.ecomp.debug");
-        errorLog = LoggerFactory.getLogger("com.att.ecomp.error");
-
-        final LoggingContext lc = getCommonLoggingContext();
-
-        String ipAddr = IP_ADDRESS;
-        String hostname = HOST_NAME;
-        try {
-            final InetAddress ip = InetAddress.getLocalHost();
-            hostname = ip.getCanonicalHostName();
-            ipAddr = ip.getHostAddress();
-        } catch (UnknownHostException x) {
-            Log.debug(x.getMessage());
-        }
-
-        lc.put("serverName", hostname);
-        lc.put("serviceName", "VESCollecor");
-        lc.put("statusCode", "RUNNING");
-        lc.put("targetEntity", "NULL");
-        lc.put("targetServiceName", "NULL");
-        lc.put("server", hostname);
-        lc.put("serverIpAddress", ipAddr);
-
-        // instance UUID is meaningless here, so we just create a new one each
-        // time the
-        // server starts. One could argue each new instantiation of the service
-        // should
-        // have a new instance ID.
-        lc.put("instanceUuid", "");
-        lc.put("severity", "");
-        lc.put(EcompFields.kEndTimestamp, SaClock.now());
-        lc.put("EndTimestamp", SaClock.now());
-        lc.put("partnerName", "NA");
-    }
-
 }
