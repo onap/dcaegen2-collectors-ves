e55ce83157b16f3efe578f75c4e6292d1c3681c4
edyta <edyta.krukowska@nokia.com>
2020-07-01 11:09:37 +0200

    Update VES Collector schema and properties
    Issue-ID: DCAEGEN2-2254
    Signed-off-by: Edyta Krukowska <edyta.krukowska@nokia.com>
    Change-Id: I6b89f4bc8ff0c2a9e51e94dc7d464608a7c153f2

--- src/test/java/org/onap/dcae/restapi/VesRestControllerTest.java
+++ src/test/java/org/onap/dcae/restapi/VesRestControllerTest.java
@@ -98,7 +98,7 @@ public class VesRestControllerTest {
         MockHttpServletRequest request = givenMockHttpServletRequest();
 
         String validEvent = new String(
-                Files.readAllBytes(Paths.get(this.getClass().getResource("/ves7_valid.json").getPath()))
+                Files.readAllBytes(Paths.get(this.getClass().getResource("/ves7_validOldEvent.json").getPath()))
         );
 
         //when
