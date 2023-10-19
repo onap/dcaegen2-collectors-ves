614d07437d96295109dbbe199afe62067fc613a6
sushant53 <sushant.jadhav@t-systems.com>
2023-10-19 13:01:28 +0530

    [DCAEGEN2] Remove DMaaP dependency in VES-Collector
    Removed DMaaP dependency in VES-Collector by using new sdk library, which
    uses Kafka API directly.
    
    Issue-ID: DCAEGEN2-3401
    Change-Id: Ia64eac12d05f71194111b82a7987283013c8a7f0
    Signed-off-by: sushant53 <sushant.jadhav@t-systems.com>

--- src/test/java/org/onap/dcae/common/publishing/DMaapContainer.java
+++ src/test/java/org/onap/dcae/common/publishing/DMaapContainer.java
@@ -4,6 +4,7 @@
  * =========================================================
  * Copyright (C) 2019-2021 Nokia. All rights reserved.
  * Copyright (C) 2023 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2023 Deutsche Telekom Intellectual Property. All rights reserved.
  * =========================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,6 +26,7 @@ import org.onap.dcae.FileReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 
 import java.io.File;
 import java.net.URI;
@@ -34,8 +36,8 @@ import java.net.URL;
 final class DMaapContainer {
     private static final String MR_COMPOSE_RESOURCE_NAME = "dmaap-msg-router/message-router-compose.yml";
     private static final String DOCKER_COMPOSE_FILE_PATH = getDockerComposeFilePath(MR_COMPOSE_RESOURCE_NAME);
-    static final int DMAAP_SERVICE_EXPOSED_PORT = 3904;
-    static final String DMAAP_SERVICE_NAME = "onap-dmaap";
+    static final int KAFKA_SERVICE_EXPOSED_PORT = 9092;
+    static final String KAFKA_SERVICE_NAME = "kafka";
     private static final Logger log = LoggerFactory.getLogger(DMaapContainer.class);
     
     private DMaapContainer() {}
@@ -51,7 +53,7 @@ final class DMaapContainer {
         }
         return new DockerComposeContainer(
                 new File(dockercomposeuri.getPath()))
-                .withExposedService(DMAAP_SERVICE_NAME, DMAAP_SERVICE_EXPOSED_PORT)
+                .withExposedService(KAFKA_SERVICE_NAME, KAFKA_SERVICE_EXPOSED_PORT, Wait.forListeningPort())
                 .withLocalCompose(true);
     }
 
